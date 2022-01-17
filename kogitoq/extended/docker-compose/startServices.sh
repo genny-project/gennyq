b14cc1d7c8486aca62beab437a9b6fae1e9a32d1
Adam Crow <adamcrow63@gmail.com>
2022-01-17 22:18:08 +1100

    Updated scripts to match ports

--- kogitoq/extended/docker-compose/startServices.sh
+++ kogitoq/extended/docker-compose/startServices.sh
@@ -82,5 +82,27 @@ else
     echo "$KOGITO_VISAS_SVG_FOLDER does not exist. Have you compiled Kogito Visas project?"
     exit 1
 fi
-
+export GENNY_SHOW_VALUES=TRUE
+export GENNY_SERVICE_USERNAME=service
+export GENNY_KEYCLOAK_URL=https://keycloak.gada.io
+export GENNY_API_URL=http://internmatch.genny.life:8280
+export GENNY_KAFKA_URL=localhost:9092
+export GENNY_CLIENT_ID=kogito-console-quarkus
+export GENNY_REALM=internmatch
+#export GENNY_KOGITO_SERVICE_URL=http://office.crowtech.com.au:8579
+echo $GENNY_KEYCLOAK_URL
+echo $GENNY_SERVICE_USERNAME
+echo $GENNY_SERVICE_PASSWORD
+echo $GENNY_CLIENT_ID
+echo $GENNY_CLIENT_SECRET
+echo $GENNY_REALM
+export GENNY_INFINISPAN_URL=localhost:11222
+echo $GENNY_INFINISPAN_URL
+echo $GENNY_INFINISPAN_USERNAME
+echo $GENNY_INFINISPAN_PASSWORD
+echo $GENNY_KAFKA_URL
+echo $GENNY_KOGITO_DATAINDEX_HTTP_URL
+echo $GENNY_KOGITO_DATAINDEX_WS_URLA
+echo $GENNY_KOGITO_JOBSERVICE_URL
+echo $GENNY_KOGITO_SERVICE_URL
 docker-compose up
