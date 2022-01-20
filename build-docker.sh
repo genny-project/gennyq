76bf72353b7ce523167d53d688727d47a7b45a48
Adam Crow <adamcrow63@gmail.com>
2022-01-20 22:40:07 +1100

    replace http with https again

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+PROJECT_VERSION=$( mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo $PROJECT_VERSION
+
 mvn clean package -Dquarkus.container-image.build=true -DskipTests=true
+docker tag gennyproject/kogitoq-visas:${PROJECT_VERSION} gennyproject/kogitoq-visas:latest
+docker tag gennyproject/kogitoq-travels:${PROJECT_VERSION} gennyproject/kogitoq-travels:latest
+docker push gennyproject/kogitoq-visas:latest
+docker push gennyproject/kogitoq-travels:latest
+
 
