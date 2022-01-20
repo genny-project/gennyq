703cb1baaf4e66d626c928c02ec7709e455fce25
Adam Crow <adamcrow63@gmail.com>
2022-01-21 10:17:54 +1100

    Added ip setting

--- kogitoq/extended/docker-compose/startServices.sh
+++ kogitoq/extended/docker-compose/startServices.sh
@@ -31,6 +31,38 @@ fi
 echo "Kogito Image version: ${KOGITO_VERSION}"
 echo "KOGITO_VERSION=${KOGITO_VERSION}" > ".env"
 
+os_type=""
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    os_type="ubuntu"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    os_type="mac"
+fi
+
+echo "OS Type is ${os_type}"
+
+if [[ "${os_type}" == "mac" ]]; then
+    IP=$(ifconfig | grep 10.123.123.123)
+elif [[ "${os_type}" == "ubuntu" ]]; then
+    IP=$(ip a | grep 10.123.123.123)
+fi
+echo $IP
+
+#if ping -c 1 10.123.123.123 &> /dev/null
+if [[ -z "$IP" ]]; then
+    if [[ ${os_type} == "mac" ]]; then
+        echo "OS TYPE: macOS"
+        sudo ifconfig lo0 alias 10.123.123.123
+    elif [[ ${os_type} == "ubuntu" ]]; then
+        echo "OS TYPE: Linux"
+        sudo ip address add 10.123.123.123 dev lo
+    else
+        echo "Can't detect OS type, neither Linux nor macOS!"
+        exit -1
+    fi
+else
+    echo "10.123.123.123 exists"
+fi
+
 if [ "$(uname)" == "Darwin" ]; then
    #echo "DOCKER_GATEWAY_HOST=${host.docker.internal}" >> ".env"
    echo "DOCKER_GATEWAY_HOST=10.123.123.123" >> ".env"
