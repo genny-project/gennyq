d20cf7c821146ddefa18ec9384e6e0a4ef6fcb34
Adam Crow <adamcrow63@gmail.com>
2022-01-30 18:38:25 +1100

    Update dto handle token

--- kogitoq/extended/travels/scripts/sendmessage.sh
+++ kogitoq/extended/travels/scripts/sendmessage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-name=$1
+messageCode=$1
+
+TOKEN=`./gettoken-prod.sh`
+echo ''
+echo $TOKEN
+echo ''
 #curl -d "messageCode=${name}" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:9580/messages?businessKey=123
-curl -H "Content-Type: application/json" -H "Accept: application/json" -X POST http://localhost:9580/messages -d @- << EOF
+#curl -H "Content-Type: application/json" --header "Authorization: Bearer $TOKEN"  -H "Accept: application/json" -X POST http://localhost:9580/messages -d @- << EOF
+curl -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://localhost:9580/messages -d @- << EOF
 {   
-        "messageCode": "${name}"
+        "messageCode": "${messageCode}"
 }
 EOF
 echo ""
