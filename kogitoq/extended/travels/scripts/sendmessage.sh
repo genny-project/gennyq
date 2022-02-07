4269b4cacf438b67d90a153b8c62540adc3a9380
Adam Crow <adamcrow63@gmail.com>
2022-02-07 22:15:56 +1100

    Updated sendmessage

--- kogitoq/extended/travels/scripts/sendmessage.sh
+++ kogitoq/extended/travels/scripts/sendmessage.sh
@@ -4,12 +4,14 @@ messageCode="${1:-MSG_IM_INTERN_LOGBOOK_REMINDER}"
 TOKEN=`./gettoken-prod.sh`
 echo ''
 echo $TOKEN
+echo "MessageCode passed is $messageCode"
 echo ''
 #curl -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://localhost:9580/messages -d @- << EOF
 #curl -d "messageCode=${name}" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:9580/messages?businessKey=123
-curl -H "Content-Type: application/json" --header "Authorization: Bearer $TOKEN"  -H "Accept: application/json" -X POST http://alyson.genny.life:9580/messages -d @- << EOF
+curl  -H "Content-Type: application/json" --header "Authorization: Bearer $TOKEN"  -H "Accept: application/json" -X POST http://alyson.genny.life:9580/messages?businessKey=123 -d @- << EOF
 {   
         "messageCode": "${messageCode}"
 }
 EOF
+
 echo ""
