68680ddb1683271043696029b70f03621a4d20c9
Adam Crow <adamcrow63@gmail.com>
2022-03-14 23:42:43 +1100

    Redid travels bpmn2 in Visual Studio Code

--- kogitoq/extended/travels/src/main/java/life/genny/kogito/live/data/InternalConsumer.java
+++ kogitoq/extended/travels/src/main/java/life/genny/kogito/live/data/InternalConsumer.java
@@ -5,100 +5,96 @@ import java.time.Instant;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
-import org.kie.api.runtime.KieSession;
-import org.kie.kogito.legacy.rules.KieRuntimeBuilder;
 
 import io.quarkus.runtime.StartupEvent;
 import io.smallrye.reactive.messaging.annotations.Blocking;
-import life.genny.serviceq.Service;
 
 @ApplicationScoped
 public class InternalConsumer {
 
-	static final Logger log = Logger.getLogger(InternalConsumer.class);
+    static final Logger log = Logger.getLogger(InternalConsumer.class);
 
-	static Jsonb jsonb = JsonbBuilder.create();
+    static Jsonb jsonb = JsonbBuilder.create();
 
-	// @Inject
-	// KieRuntimeBuilder ruleRuntime;
+    // @Inject
+    // KieRuntimeBuilder ruleRuntime;
 
-	// @Inject
-	// Service service;
+    // @Inject
+    // Service service;
 
-	// KieSession ksession;
+    // KieSession ksession;
 
-	/**
-	 * Execute on start up.
-	 *
-	 * @param ev
-	 */
-	void onStart(@Observes StartupEvent ev) {
+    /**
+     * Execute on start up.
+     *
+     * @param ev
+     */
+    void onStart(@Observes StartupEvent ev) {
 
-		// service.fullServiceInit();
-		log.info("[*] Finished Startup!");
-	}
+        // service.fullServiceInit();
+        log.info("[*] Finished Startup!");
+    }
 
-	/**
-	 * Consume from the valid_data topic.
-	 *
-	 * @param data
-	 */
-	@Incoming("events")
-	@Blocking
-	public void getEvent(String data) {
+    /**
+     * Consume from the valid_data topic.
+     *
+     * @param data
+     */
+    @Incoming("events")
+    @Blocking
+    public void getEvent(String data) {
 
-		log.info("Incoming Event : {}" + data);
-		Instant start = Instant.now();
+        log.info("Incoming Event : {}" + data);
+        Instant start = Instant.now();
 
-		// BaseEntityUtils beUtils = service.getBeUtils();
-		// GennyToken serviceToken = beUtils.getServiceToken();
-		// GennyToken userToken = null;
+        // BaseEntityUtils beUtils = service.getBeUtils();
+        // GennyToken serviceToken = beUtils.getServiceToken();
+        // GennyToken userToken = null;
 
-		// // deserialise to msg
-		// QDataAnswerMessage msg = jsonb.fromJson(data, QDataAnswerMessage.class);
+        // // deserialise to msg
+        // QDataAnswerMessage msg = jsonb.fromJson(data, QDataAnswerMessage.class);
 
-		// // check the token
-		// String token = msg.getToken();
-		// try {
-		// userToken = new GennyToken(token);
-		// } catch (Exception e) {
-		// log.error("Invalid Token!");
-		// return;
-		// }
+        // // check the token
+        // String token = msg.getToken();
+        // try {
+        // userToken = new GennyToken(token);
+        // } catch (Exception e) {
+        // log.error("Invalid Token!");
+        // return;
+        // }
 
-		// // update the token of our utility
-		// beUtils.setGennyToken(userToken);
+        // // update the token of our utility
+        // beUtils.setGennyToken(userToken);
 
-		// Answer answer = msg.getItems()[0];
-		// Answers answersToSave = new Answers();
+        // Answer answer = msg.getItems()[0];
+        // Answers answersToSave = new Answers();
 
-		// // init session and activate DataProcessing
-		// KieSession ksession = ruleRuntime.newKieSession();
-		// ((InternalAgenda)
-		// ksession.getAgenda()).activateRuleFlowGroup("DataProcessing");
+        // // init session and activate DataProcessing
+        // KieSession ksession = ruleRuntime.newKieSession();
+        // ((InternalAgenda)
+        // ksession.getAgenda()).activateRuleFlowGroup("DataProcessing");
 
-		// // insert facts into session
-		// ksession.insert(beUtils);
-		// ksession.insert(serviceToken);
-		// ksession.insert(userToken);
-		// ksession.insert(answer);
-		// ksession.insert(answersToSave);
+        // // insert facts into session
+        // ksession.insert(beUtils);
+        // ksession.insert(serviceToken);
+        // ksession.insert(userToken);
+        // ksession.insert(answer);
+        // ksession.insert(answersToSave);
 
-		// // fire rules and dispose of session
-		// ksession.fireAllRules();
-		// ksession.dispose();
+        // // fire rules and dispose of session
+        // ksession.fireAllRules();
+        // ksession.dispose();
 
-		// // TODO: ensure that answersToSave has been updated by our rules
-		// beUtils.saveAnswers(answersToSave);
+        // // TODO: ensure that answersToSave has been updated by our rules
+        // beUtils.saveAnswers(answersToSave);
 
-		Instant end = Instant.now();
-		log.info("Duration = " + Duration.between(start, end).toMillis() + "ms");
-	}
+        Instant end = Instant.now();
+        log.info("Duration = " + Duration.between(start, end).toMillis() + "ms");
+    }
 
 }
