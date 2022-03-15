f2f8dbdb05d4297394bf921c73c8e2ae19885f1c
Adam Crow <adamcrow63@gmail.com>
2022-03-15 15:55:36 +1100

    Updated

--- kogitoq/extended/travels/scripts/abort-noidc.sh
+++ kogitoq/extended/travels/scripts/abort-noidc.sh
@@ -8,14 +8,6 @@ echo ''
 echo $TOKEN
 echo "MessageCode3 passed is $messageCode - testing BaseEntity"
 echo ''
-#curl -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://localhost:9580/messages -d @- << EOF
-#curl  --header "Authorization: Bearer $TOKEN"  -d "messageCode={\"code\":\"${messageCode}\",\"bookingNumber\":\"${bookingNumber}\"}" -H "Content-Type: application/json" -X POST http://localhost:9580/messages?businessKey=123
-#curl  -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://alyson.genny.life:9580/messages?businessKey=${bookingNumber} -d @- << EOF
-curl   -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://alyson2.genny.life:${port}/travels/{id}/ABORT_SIGNAL -d @- << EOF
-{
-     "abortCode" : {
-     }
-}
-EOF
+curl   -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://alyson2.genny.life:${port}/travels/${id}/ABORT_SIGNAL 
 
 echo ""
