92dd9c51d006769008853d58d01026551e8d8911
varunshastry <varunshastry@rediffmail.com>
2022-03-16 10:38:54 +0530

    Parameterizing abort-noidc.sh to pick process instance id

--- kogitoq/extended/travels/scripts/abort-noidc.sh
+++ kogitoq/extended/travels/scripts/abort-noidc.sh
@@ -1,64 +1,12 @@
 #!/bin/bash
-abortCode="${1:-ABORT_SIGNAL}"
-port="${2:-8580}"
+abortCode="${2:-abortsignal}"
+port="${3:-8580}"
 bookingNumber=123
-id=2d7648dd-d570-45ee-98b2-059aff2570c7
+id=$1 
 TOKEN=`./gettoken-prod.sh`
 echo ''
 echo $TOKEN
 echo "MessageCode3 passed is $messageCode - testing BaseEntity"
 echo ''
-curl   -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://alyson.genny.life:${port}/travels/${id}/ABORT_SIGNAL/ -d @- << EOF
-{
-  "id": "string",
-  "flight": {
-    "flightNumber": "string",
-    "seat": "string",
-    "gate": "string",
-    "departure": "2022-03-15",
-    "arrival": "2022-03-15"
-  },
-  "trip": {
-    "city": "string",
-    "country": "string",
-    "begin": "2022-03-15",
-    "end": "2022-03-15",
-    "visaRequired": true
-  },
-  "hotel": {
-    "name": "string",
-    "address": {
-      "street": "string",
-      "city": "string",
-      "zipCode": "string",
-      "country": "string"
-    },
-    "phone": "string",
-    "bookingNumber": "string",
-    "room": "string"
-  },
-  "visaApplication": {
-    "firstName": "string",
-    "lastName": "string",
-    "city": "string",
-    "country": "string",
-    "duration": 0,
-    "passportNumber": "string",
-    "nationality": "string",
-    "approved": true
-  },
-  "traveller": {
-    "firstName": "string",
-    "lastName": "string",
-    "email": "string",
-    "nationality": "string",
-    "address": {
-      "street": "string",
-      "city": "string",
-      "zipCode": "string",
-      "country": "string"
-    }
-  }
-}
-EOF
-echo ""
+curl   -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://alyson2.genny.life:${port}/travels/${id}/${abortCode} -d ''
+echo "Done..!!"
