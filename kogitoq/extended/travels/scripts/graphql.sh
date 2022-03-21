2f1cc8679dab9c91f34178d4d7ef5290a5414a8e
Adam Crow <adamcrow63@gmail.com>
2022-03-21 17:46:53 +1100

    Got graphql script running.

--- kogitoq/extended/travels/scripts/graphql.sh
+++ kogitoq/extended/travels/scripts/graphql.sh
@@ -3,18 +3,13 @@ TOKEN=`./gettoken-prod.sh`
 echo ''
 echo $TOKEN
 echo ''
-curl   -H "Content-Type: application/json"  -H "Accept: application/json" -X POST http://alyson.genny.life:8582 -d @- << EOF
-
-{
-  Travels:(where: {Application: {agentCode: {like: "PER_086CDF1F-A98F-4E73-9825-0A4CFE2BB943*"}}}) {
-    Application {
-      internCode 
-      agentCode 
-    }
-  }
+curl   -H "Content-Type: application/GraphQL"  -H "Accept: application/json" -X POST http://alyson.genny.life:8582/graphql -d @- << EOF
+query {
+  Application(where: {internCode: { like: "PER_A%" }}) {
+    id
+    internCode
+    agentCode
+  } 
 }
-
-
 EOF
-
 echo ""
