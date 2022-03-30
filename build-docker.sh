9b761ff93a39c3dae7b92a69127fe4aa2f2eb879
Jasper Robison <jasper.robison22@gmail.com>
2022-03-30 16:30:47 +1100

    output with color

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,7 @@
 PROJECT_VERSION=$( mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo $PROJECT_VERSION
 
-mvn clean package -Dquarkus.container-image.build=true -DskipTests=true
+mvn clean package -Dquarkus.container-image.build=true -DskipTests=true -Dstyle.color=always
 docker tag gennyproject/kogitoq-visas:${PROJECT_VERSION} gennyproject/kogitoq-visas:latest
 docker tag gennyproject/kogitoq-travels:${PROJECT_VERSION} gennyproject/kogitoq-travels:latest
 docker push gennyproject/kogitoq-visas:latest
