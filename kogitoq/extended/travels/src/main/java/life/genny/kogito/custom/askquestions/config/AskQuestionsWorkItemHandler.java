f9a06a5b15a4b6d2e23b4ab807261ebf125f8883
Adam Crow <adamcrow63@gmail.com>
2022-03-30 20:30:14 +1100

    Updated to GEN-721

--- kogitoq/extended/travels/src/main/java/life/genny/kogito/custom/askquestions/config/AskQuestionsWorkItemHandler.java
+++ kogitoq/extended/travels/src/main/java/life/genny/kogito/custom/askquestions/config/AskQuestionsWorkItemHandler.java
@@ -8,14 +8,12 @@ import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.persistence.EntityManager;
 
-import life.genny.qwandaq.Ask;
 import org.apache.commons.lang3.StringUtils;
 import org.jboss.logging.Logger;
 import org.kie.kogito.internal.process.runtime.KogitoWorkItem;
 import org.kie.kogito.internal.process.runtime.KogitoWorkItemHandler;
 import org.kie.kogito.internal.process.runtime.KogitoWorkItemManager;
 
-import life.genny.qwandaq.Ask;
 import life.genny.qwandaq.entity.BaseEntity;
 import life.genny.qwandaq.entity.SearchEntity;
 import life.genny.qwandaq.models.GennyToken;
@@ -98,8 +96,9 @@ public class AskQuestionsWorkItemHandler implements KogitoWorkItemHandler {
                 // Create the Ask
 
                 // questionUtils.sendQuestions(recipient, userToken);
-                List<Ask> asks = findAsksByQuestionGroupCode("internmatch", "QUE_ADMIN_GRP");
-                log.info("asks:" + asks);
+                // List<Ask> asks = databaseUtils.findAsksByQuestionGroupCode("internmatch",
+                // "QUE_ADMIN_GRP");
+                // log.info("asks:" + asks);
 
                 // Set up a UserTask
             }
