59d8b2c6a1ed05a556113f1a02735fab0dc792f6
Adam Crow <adamcrow63@gmail.com>
2022-04-09 21:54:18 +1000

    Updated script saveanswer2

--- kogitoq/extended/travels/scripts/sendanswer.sh
+++ kogitoq/extended/travels/scripts/sendanswer.sh
@@ -6,7 +6,7 @@ echo "Value=${value}"
 #TOKEN=`./gettoken-prod.sh`
 TOKEN=`./gettoken-cache.sh`
 echo ''
-echo $TOKEN
+#echo $TOKEN
 echo ''
 attributeCode=PRI_LASTNAME
 questionCode=QUE_LASTNAME
@@ -14,7 +14,7 @@ sourceCode=PER_086CDF1F-A98F-4E73-9825-0A4CFE2BB943
 targetCode=PER_34EB0455-1DC0-4121-80ED-90C0B9EEA413
 
 payload="{\"items\":[{\"askId\":349572,\"processId\":\"${processId}\",\"attributeCode\":\"${attributeCode}\",\"sourceCode\":\"PER_086CDF1F-A98F-4E73-9825-0A4CFE2BB943\",\"targetCode\":\"${targetCode}\",\"code\":\"${questionCode}\",\"identifier\":\"${questionCode}\",\"weight\":1,\"value\":\"${value}\",\"inferred\":false}],\"token\":\"${TOKEN}\",\"msg_type\":\"DATA_MSG\",\"event_type\":false,\"redirect\":false,\"data_type\":\"Answer\"}"
-echo $payload
+#echo $payload
 echo  $payload > answer.json
 
 
@@ -23,4 +23,4 @@ docker exec -i kafka bash -c " \
 printf '%s\n%s\n' 'security.protocol=PLAINTEXT' 'sasl.mechanism=PLAIN' > prop && \
 kafka-console-producer --producer.config=prop  --bootstrap-server localhost:9092 --topic valid_data" < answer.json
 
-echo ""
+echo "Answer sent"
