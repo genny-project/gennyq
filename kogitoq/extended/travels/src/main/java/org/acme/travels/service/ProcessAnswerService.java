dafab37434419b9a189ce0bbe5b03d4c5a08f863
Adam Crow <adamcrow63@gmail.com>
2022-04-09 22:54:08 +1000

    Added submit processing and saving

--- kogitoq/extended/travels/src/main/java/org/acme/travels/service/ProcessAnswerService.java
+++ kogitoq/extended/travels/src/main/java/org/acme/travels/service/ProcessAnswerService.java
@@ -10,9 +10,13 @@ import javax.persistence.EntityManager;
 import org.jboss.logging.Logger;
 
 import life.genny.qwandaq.Answer;
+import life.genny.qwandaq.Ask;
+import life.genny.qwandaq.attribute.EntityAttribute;
 import life.genny.qwandaq.entity.BaseEntity;
 import life.genny.qwandaq.exception.BadDataException;
 import life.genny.qwandaq.message.QDataAnswerMessage;
+import life.genny.qwandaq.message.QDataAskMessage;
+import life.genny.qwandaq.utils.BaseEntityUtils;
 import life.genny.qwandaq.utils.DatabaseUtils;
 import life.genny.qwandaq.utils.QuestionUtils;
 import life.genny.qwandaq.utils.QwandaUtils;
@@ -75,6 +79,47 @@ public class ProcessAnswerService {
         log.info("Saved Value into processBE " + answer.getAttributeCode() + " as " + savedValue);
 
         return processBE;
+    }
+
+    public Boolean checkMandatory(String qDataAskMessageJson, String processBEJson) {
+
+        QDataAskMessage qDataAskMessage = jsonb.fromJson(qDataAskMessageJson, QDataAskMessage.class);
+        BaseEntity processBE = jsonb.fromJson(processBEJson, BaseEntity.class);
+        Boolean mandatoryUnanswered = false;
+
+        for (Ask ask : qDataAskMessage.getItems()) {
+            Object val = processBE.getValue(ask.getAttributeCode(), null);
+            if (val == null) {
+                if (ask.getMandatory().equals(Boolean.TRUE)) {
+                    mandatoryUnanswered = true;
+                    return false;
+                }
+            }
+        }
+
+        return !mandatoryUnanswered;
+    }
+
+    public void saveAllAnswers(String sourceCode, String targetCode, String processBEJson) {
+        BaseEntityUtils beUtils = new BaseEntityUtils(service.getServiceToken());
+
+        BaseEntity processBE = jsonb.fromJson(processBEJson, BaseEntity.class);
+
+        BaseEntity source = beUtils.getBaseEntityByCode(sourceCode);
+        BaseEntity target = beUtils.getBaseEntityByCode(targetCode);
+        // Now to go through all the fields and override them in the target BE
+        for (EntityAttribute ea : processBE.getBaseEntityAttributes()) {
+            // try {
+            Answer ans = new Answer(source, target, ea.getAttribute(), ea.getValueString());
+            ans.setWeight(ea.getWeight());
+            beUtils.saveAnswer(ans);
+            // target.setValue(ea.getAttributeCode(), ea.getValue(), ea.getValueDouble());
+            // } catch (BadDataException e) {
+            // // TODO Auto-generated catch block
+            // e.printStackTrace();
+            // }
+        }
+
     }
 
 }
