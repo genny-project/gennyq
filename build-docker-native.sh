0b4ec69dca4e36cfeb729af43b2c8476e9a8e4a3
Jasper Robison <jasper.robison22@gmail.com>
2022-04-12 15:06:44 +1000

    build docker script additions

---
+++ build-docker-native.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VERSION=$( mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "Project Version: $VERSION"
+
+if [ "$#" -eq 1 ]; then
+	./mvnw clean package -Pnative -Dquarkus.native.remote-container-build=true -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-mandrel:21.3-java11 -DskipTests=true -pl :$1
+	# docker tag gennyproject/${1}:${VERSION}-native gennyproject/${1}:latest
+	exit 0
+fi
+
+./mvnw clean package -Pnative -Dquarkus.native.remote-container-build=true -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-mandrel:21.3-java11 -DskipTests=true
+
+for project in kogitoq-travels kogitoq-visas adi bridge fyodor dropkick lauchy messages
+do
+    echo "Tagging $project"
+	docker tag gennyproject/${project}:${VERSION} gennyproject/${project}:latest
+done
+
+
+
+
+
+
+
+
+
+# export JAVA_HOME=$GRAALVM_HOME
+# export PATH=$JAVA_HOME/bin:$PATH
+# java -version
+
+
+# docker build -f src/main/docker/Dockerfile.native -t  gennyproject/fyodor:${VERSION_TO_BUILD}-native .
+
+
--- build-docker.sh
+++ build-docker.sh
@@ -3,7 +3,13 @@
 VERSION=$( mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo "Project Version: $VERSION"
 
-mvn clean package -Dquarkus.container-image.build=true -DskipTests=true -Dstyle.color=always
+if [ "$#" -eq 1 ]; then
+	./mvnw clean package -Dquarkus.container-image.build=true -DskipTests=true -Dstyle.color=always -pl :$1
+	docker tag gennyproject/${1}:${VERSION} gennyproject/${1}:latest
+	exit 0
+fi
+
+./mvnw clean package -Dquarkus.container-image.build=true -DskipTests=true -Dstyle.color=always
 
 for project in kogitoq-travels kogitoq-visas adi bridge fyodor dropkick lauchy messages
 do
