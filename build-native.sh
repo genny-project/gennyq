e0dcc3cd185a1d09c8e43186f9e6a521cb102e1f
Jasper Robison <jasper.robison22@gmail.com>
2022-04-12 13:56:58 +1000

    update build scripts for single project builds

--- build-native.sh
+++ build-native.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+if [ "$#" -eq 1 ]; then
+	./mvnw clean package -Pnative -DskipTests=true -Dcheckstyle.skip -pl :$1
+	exit 1
+fi
+
 ./mvnw clean package -Pnative -DskipTests=true -Dcheckstyle.skip 
+
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -eq 1 ]; then
-	./mvnw clean package -DskipTests=true -Dcheckstyle.skip -rf :$1
+	./mvnw clean package -DskipTests=true -Dcheckstyle.skip -pl :$1
 	exit 1
 fi
 
