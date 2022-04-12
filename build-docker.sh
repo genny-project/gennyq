450c71dc6c2fe01cb1c7e1345afb41dec6c9d732
Jasper Robison <jasper.robison22@gmail.com>
2022-04-13 09:07:23 +1000

    messing with individual build

--- build-docker.sh
+++ build-docker.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-clean=${1:-}
-VERSION=$( ./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
+VERSION=$( mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo "Project Version: $VERSION"
 
 if [ "$#" -eq 1 ]; then
-	./mvnw ${clean}  package -Dquarkus.container-image.build=true -DskipTests=true -Dstyle.color=always -pl :$1
+	./mvnw clean  package -Dquarkus.container-image.build=true -DskipTests=true -Dstyle.color=always -pl :$1
 	docker tag gennyproject/${1}:${VERSION} gennyproject/${1}:latest
 	exit 0
 fi
 
-./mvnw ${clean} package -Dquarkus.container-image.build=true -DskipTests=true -Dstyle.color=always
+./mvnw clean package -Dquarkus.container-image.build=true -DskipTests=true -Dstyle.color=always
 
 for project in kogitoq-travels kogitoq-visas adi bridge fyodor dropkick lauchy messages
 do
