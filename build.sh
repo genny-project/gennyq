2ed5093c798bd370c3dbf3272a83aea93db0cb68
Shannon <shannon@gada.io>
2022-04-13 07:00:38 +0000

    Removing 'clean' from build script

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+project=$1
+
 if [ "$#" -eq 1 ]; then
-	./mvnw clean package -DskipTests=true -Dcheckstyle.skip -Dstyle.color=always -pl :$1
+	./mvnw package -DskipTests=true -Dcheckstyle.skip -Dstyle.color=always -pl :$1
 	exit 0
 fi
 
 echo "Building all sub-projects!"
-./mvnw clean package -DskipTests=true -Dcheckstyle.skip -Dstyle.color=always 
+./mvnw package -DskipTests=true -Dcheckstyle.skip -Dstyle.color=always 
