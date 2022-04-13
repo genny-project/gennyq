83a37dcc584405d138ba82e80fcfa599a4c929dd
Jasper Robison <jasper.robison22@gmail.com>
2022-04-13 12:22:21 +1000

    add shleemy files

---
+++ shleemy/src/main/java/life/genny/shleemy/endpoints/ScheduleResource.java
@@ -0,0 +1,220 @@
+package life.genny.shleemy.endpoints;
+
+import java.net.URI;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+import org.quartz.SchedulerException;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.security.identity.SecurityIdentity;
+import life.genny.qwandaq.models.GennyToken;
+import life.genny.qwandaq.message.QScheduleMessage;
+import life.genny.shleemy.quartz.TaskBean;
+
+@Path("/api/schedule")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ScheduleResource {
+
+	private static final Logger log = Logger.getLogger(ScheduleResource.class);
+
+	@ConfigProperty(name = "default.realm", defaultValue = "genny")
+	String defaultRealm;
+
+	@Inject
+	JsonWebToken accessToken;
+
+	@Inject
+	TaskBean taskBean;
+
+	void onStart(@Observes StartupEvent ev) {
+		log.info("ScheduleResource Endpoint starting");
+	}
+
+	void onShutdown(@Observes ShutdownEvent ev) {
+		log.info("ScheduleResource Endpoint Shutting down");
+	}
+
+	@OPTIONS
+	public Response opt() {
+		return Response.ok().build();
+	}
+
+	@POST
+	@Transactional
+	public Response newQScheduleMessage(@Context UriInfo uriInfo, @Valid QScheduleMessage scheduleMessage) {
+
+		String uniqueScheduleCode = "";
+
+		scheduleMessage.id = null;
+		GennyToken userToken = new GennyToken(accessToken.getRawToken());
+		log.info("User is " + userToken.getEmail());
+
+		scheduleMessage.realm = userToken.getRealm();
+		scheduleMessage.sourceCode = userToken.getUserCode();
+		scheduleMessage.token = userToken.getToken();
+		scheduleMessage.persist();
+		
+		log.info("Persisting new Schedule -> "+scheduleMessage.code+":"+scheduleMessage.triggertime+" from "+scheduleMessage.sourceCode);
+		
+		try {
+			uniqueScheduleCode = taskBean.addSchedule(scheduleMessage, userToken);
+			URI uri = uriInfo.getAbsolutePathBuilder().path(ScheduleResource.class, "findById")
+					.build(scheduleMessage.id);
+			return Response.created(uri).entity(uniqueScheduleCode).build();
+
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+		}
+
+		return Response.status(Status.BAD_REQUEST).entity("ScheduleMessage did not schedule").build();
+	}
+
+	@GET
+	@Path("/code/{code}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response findByCode(@PathParam("code") final String code) {
+		GennyToken userToken = new GennyToken(accessToken.getRawToken());
+		log.info("User is " + userToken.getEmail());
+
+		QScheduleMessage scheduleMessage = QScheduleMessage.findByCode(code);
+		if (scheduleMessage == null) {
+			return Response.status(Status.NOT_FOUND).entity("ScheduleMessage with code of " + code + " does not exist.")
+					.build();
+		}
+		if (scheduleMessage.realm != userToken.getRealm()) {
+			return Response.status(Status.NOT_FOUND).entity("ScheduleMessage with bad realm")
+					.build();
+		}
+
+		return Response.status(Status.OK).entity(scheduleMessage).build();
+	}
+	
+	@GET
+	@Path("/id/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response findById(@PathParam("id") final Long id) {
+
+		GennyToken userToken = new GennyToken(accessToken.getRawToken());
+		log.info("User is " + userToken.getEmail());
+
+		QScheduleMessage scheduleMessage = QScheduleMessage.findById(id);
+		if (scheduleMessage == null) {
+			return Response.status(Status.NOT_FOUND).entity("ScheduleMessage with id of " + id + " does not exist.")
+					.build();
+		}
+		if (scheduleMessage.realm != userToken.getRealm()) {
+			return Response.status(Status.NOT_FOUND).entity("ScheduleMessage with id of " + id + " does not exist.")
+					.build();
+		}
+
+		return Response.status(Status.OK).entity(scheduleMessage).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Transactional
+	public Response deleteSchedule(@PathParam("id") final Long id) {
+
+		GennyToken userToken = new GennyToken(accessToken.getRawToken());
+		log.info("User is " + userToken.getEmail());
+
+		QScheduleMessage scheduleMessage = QScheduleMessage.findById(id);
+		if (scheduleMessage == null) {
+			return Response.status(Status.NOT_FOUND).entity("ScheduleMessage with id of " + id + " does not exist.")
+					.build();
+		}
+
+		if (scheduleMessage.realm != userToken.getRealm()) {
+			return Response.status(Status.NOT_FOUND).entity("ScheduleMessage with id of " + id + " does not exist.")
+					.build();
+		}
+
+		if (!(userToken.hasRole("admin")) && !(userToken.getUserCode().equals(scheduleMessage.sourceCode))) {
+			return Response.status(Status.FORBIDDEN)
+					.entity("ScheduleMessage with id of " + id + " cannot be deleted by this user.").build();
+		}
+
+		// find msg using id
+		QScheduleMessage msg = QScheduleMessage.findById(id);
+		if (msg == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+
+		// attempt to abort schedule
+		try {
+			taskBean.abortSchedule(msg.code, userToken);
+
+		} catch (org.quartz.SchedulerException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+
+			return Response.status(Status.NOT_FOUND).build();
+		}
+
+		// delete schedule and return ok status
+		QScheduleMessage.deleteByCode(msg.code);
+		return Response.status(Status.OK).build();
+	}
+
+	@Transactional
+	@DELETE
+	@Path("/code/{code}")
+	public Response deleteSchedule(@PathParam("code") final String code) {
+
+		GennyToken userToken = new GennyToken(accessToken.getRawToken());
+		log.info("User is " + userToken.getEmail());
+
+		QScheduleMessage scheduleMessage = QScheduleMessage.findByCode(code);
+		if (scheduleMessage == null) {
+			return Response.status(Status.NOT_FOUND).entity("ScheduleMessage with code of " + code + " does not exist.")
+					.build();
+		}
+
+		if (scheduleMessage.realm != userToken.getRealm()) {
+			return Response.status(Status.NOT_FOUND).entity("ScheduleMessage has a different realm to user.").build();
+		}
+
+		if (!(userToken.hasRole("admin")) && !(userToken.getUserCode().equals(scheduleMessage.sourceCode))) {
+			return Response.status(Status.FORBIDDEN)
+					.entity("ScheduleMessage with code of " + code + " cannot be deleted by this user.").build();
+		}
+
+		// attempt to abort schedule
+		try {
+			taskBean.abortSchedule(code, userToken);
+
+		} catch (org.quartz.SchedulerException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+
+			return Response.status(Status.NOT_FOUND).build();
+		}
+
+		// delete schedule and return ok status
+		QScheduleMessage.deleteByCode(code);
+		return Response.status(Status.OK).build();
+	}
+
+}
---
+++ shleemy/src/main/java/life/genny/shleemy/quartz/TaskBean.java
@@ -0,0 +1,132 @@
+package life.genny.shleemy.quartz;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import io.quarkus.runtime.StartupEvent;
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.jboss.logging.Logger;
+import org.quartz.*;
+
+import life.genny.shleemy.endpoints.ScheduleResource;
+import life.genny.qwandaq.models.GennyToken;
+import life.genny.qwandaq.message.QScheduleMessage;
+import life.genny.shleemy.live.data.InternalProducer;
+import life.genny.serviceq.Service;
+
+@ApplicationScoped
+public class TaskBean {
+
+    private static final Logger log = Logger.getLogger(ScheduleResource.class);
+
+    @Inject
+    org.quartz.Scheduler quartz;
+
+    @Inject
+    InternalProducer producer;
+
+	@Inject
+	Service service;
+
+    void onStart(@Observes StartupEvent event) {
+    }
+
+    public void abortSchedule(String uniqueCode, GennyToken userToken) throws SchedulerException {
+
+        JobKey jobKey = new JobKey(uniqueCode, userToken.getRealm());
+        quartz.deleteJob(jobKey);
+    }
+
+    public String addSchedule(QScheduleMessage scheduleMessage, GennyToken userToken) throws SchedulerException {
+
+        scheduleMessage.token = userToken.getToken();
+
+        String messageJson = scheduleMessage.jsonMessage;
+
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put("message", messageJson);
+        jobDataMap.put("sourceCode", userToken.getUserCode());
+        jobDataMap.put("token", userToken.getToken());
+        jobDataMap.put("channel", scheduleMessage.channel);
+        jobDataMap.put("code", scheduleMessage.code);
+
+        String uniqueCode = scheduleMessage.code;
+
+        JobDetail job = JobBuilder.newJob(MyJob.class).withIdentity(uniqueCode, userToken.getRealm())
+                .setJobData(jobDataMap).build();
+
+        Trigger trigger = null;
+        log.info("scheduleMessage: " + scheduleMessage);
+
+		// handle cron trigger
+        if (!scheduleMessage.cron.isBlank()) {
+
+			CronScheduleBuilder cronSchedule = CronScheduleBuilder.cronSchedule(scheduleMessage.cron);
+            trigger = TriggerBuilder.newTrigger()
+					.withIdentity(uniqueCode, userToken.getRealm())
+					.startNow()
+                    .withSchedule(cronSchedule)
+                    .build();
+
+            log.info("Scheduled " + userToken.getUserCode() + ":" + userToken.getEmail() + " for " + userToken.getRealm()
+                            + " for trigger at " + scheduleMessage.cron + " and now is " + LocalDateTime.now());
+
+		// handle time trigger
+        } else if (scheduleMessage.triggertime != null) {
+
+            Date scheduledDateTime = Date.from(scheduleMessage.triggertime.atZone(ZoneId.systemDefault()).toInstant());
+            trigger = TriggerBuilder.newTrigger()
+					.withIdentity(uniqueCode, userToken.getRealm())
+                    .startAt(scheduledDateTime)
+                    .forJob(uniqueCode, userToken.getRealm())
+                    .build();
+
+            log.info("Scheduled " + userToken.getUserCode() + ":" + uniqueCode + ":" + userToken.getEmail() + " for " + userToken.getRealm()
+                            + " for trigger at " + scheduledDateTime + " and now is " + LocalDateTime.now());
+        }
+
+        quartz.scheduleJob(job, trigger);
+
+        return uniqueCode;
+    }
+
+    @Transactional
+    void performTask(JobExecutionContext context) {
+
+        log.info("Executing Scheduler Task: " + context.getFireTime());
+        String bridgeUrl = ConfigProvider.getConfig().getValue("bridge.service.url", String.class);
+
+        String sourceCode = context.getJobDetail().getJobDataMap().getString("sourceCode");
+        String channel = context.getJobDetail().getJobDataMap().getString("channel");
+        String code = context.getJobDetail().getJobDataMap().getString("code");
+        String token = context.getJobDetail().getJobDataMap().getString("token");
+        GennyToken userToken = new GennyToken(token);
+
+        String scheduleMsgJson = (String) context.getJobDetail().getJobDataMap().get("message");
+
+        log.info(scheduleMsgJson);
+
+        producer.getToEvents().send(scheduleMsgJson);
+
+        log.info("Executing Schedule " + sourceCode + ":" + code + ":" + userToken.getEmail() + " for " + userToken.getRealm()
+                + " at " + LocalDateTime.now() + " sending through bridgeUrl=" + bridgeUrl + ", scheduleMsgJson:" + scheduleMsgJson);
+    }
+
+    // A new instance of MyJob is created by Quartz for every job execution
+    public static class MyJob implements Job {
+
+        @Inject
+        TaskBean taskBean;
+
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            taskBean.performTask(context);
+        }
+
+    }
+}
