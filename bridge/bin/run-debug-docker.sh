620b854d8627726ef3a6b19c30fdb213f7daea11
Expressionless <rhysmchaen@gmail.com>
2022-05-05 12:18:32 +1000

    removed bin folder from bridge that keeps autogenning. Will need to add to gitignore probably

--- bridge/bin/run-debug-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mvn quarkus:remote-dev -Ddebug=false \
-  -Dquarkus.package.type=mutable-jar \
-  -Dquarkus.live-reload.url=http://localhost:8080 \
-  -Dquarkus.live-reload.password=123
-
