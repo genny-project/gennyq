637e20341b489c4d107db0e6b601c386541d8ad4
Jasper Robison <jasper.robison22@gmail.com>
2022-05-10 10:07:58 +1000

    log token

--- kogitoq/gadaq/src/main/java/life/genny/gadaq/utils/KogitoUtils.java
+++ kogitoq/gadaq/src/main/java/life/genny/gadaq/utils/KogitoUtils.java
@@ -154,6 +154,8 @@ public class KogitoUtils {
         String url = kogitoServiceUrl + "/" + process.toLowerCase();
         String jsonStr = jsonb.toJson(message);
 
+		log.info("TOKEN = " + userToken.getToken());
+
         String workflowJsonStr = "{\"eventMessage\":" + jsonStr + ", \"token\":\"" + userToken.getToken() + "\"}";
 
         HttpResponse<String> response = HttpUtils.post(url, workflowJsonStr, null);
