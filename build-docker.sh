516a7ed5be311b92afddba747671c650f684b54b
Jasper Robison <jasper.robison22@gmail.com>
2022-05-12 16:04:59 +1000

    force delete the target dir before building

--- build-docker.sh
+++ build-docker.sh
@@ -10,10 +10,19 @@ else
 	projects=( qwandaq serviceq gadaq bridge fyodor dropkick lauchy messages shleemy )
 fi
 
+# iterate projects
 for project in "${projects[@]}"
 do
     echo "Building $project"
+
+	# remove target dir
+	rm -rf $project/target
+	rm -rf kogito/$project/target
+
+	# perform clean install
 	./mvnw clean install -Dquarkus.container-image.build=true -DskipTests=true -Dcheckstyle.skip -Dstyle.color=always -pl :$project
+
+	# tag the docker container
     echo "Tagging $project"
 	docker tag gennyproject/${project}:${VERSION} gennyproject/${project}:latest
 done
--- build.sh
+++ build.sh
@@ -6,9 +6,16 @@ else
 	projects=( qwandaq serviceq gadaq bridge fyodor dropkick lauchy messages shleemy )
 fi
 
+# iterate projects
 for project in "${projects[@]}"
 do
     echo "Building $project"
+
+	# remove target dir
+	rm -rf $project/target
+	rm -rf kogito/$project/target
+
+	# perform clean install
 	./mvnw clean install -DskipTests=true -Dcheckstyle.skip -Dstyle.color=always -pl :$project
 done
 
