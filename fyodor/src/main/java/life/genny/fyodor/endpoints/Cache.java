b1b2284bf53f9658cc694cbcbb73f2bdefd5bec1
Adam Crow <adamcrow63@gmail.com>
2022-05-28 13:22:43 +1000

    Added token fetch for old rulesservice

--- fyodor/src/main/java/life/genny/fyodor/endpoints/Cache.java
+++ fyodor/src/main/java/life/genny/fyodor/endpoints/Cache.java
@@ -114,7 +114,11 @@ if (res.getStatus() == 200) {
 		}
 
 		if (!userToken.hasRole("test", "service")) {
-			log.error("User ["+userToken.userCode+"] does not have valid role:"+userToken.getUserRoles());
+			log.error("User [" + userToken.userCode + "] does not have valid role:" + userToken.getUserRoles());
+			// TODO -> Do not permit access from externally
+			if ("jenny".equals(productCode) && (key.startsWith("TOKEN"))) {
+				return Response.ok(serviceToken.getToken()).build();
+			}
 			return Response.status(Response.Status.BAD_REQUEST)
 				.entity(HttpUtils.error("User not authorized to make this request")).build();
 		}
