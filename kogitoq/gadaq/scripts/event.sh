10b3d1842f67fcf5ddddac42f596a4b28b4437ba
Jasper Robison <jasper.robison22@gmail.com>
2022-06-09 09:05:25 +1000

    event script

---
+++ kogitoq/gadaq/scripts/event.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TOKEN=${1}
+sourceCode=${2}
+targetCode=${3}
+questionCode=${4}
+
+payload="{\"data\":{\"sourceCode\":\"${sourceCode}\",\"targetCode\":\"${targetCode}\",\"code\":\"${questionCode}\"},\"token\":\"${TOKEN}\",\"msg_type\":\"EVT_MSG\"}"
+#echo $payload
+echo  $payload > answer.json
+
+docker exec -i kafka bash -c " \
+printf '%s\n%s\n' 'security.protocol=PLAINTEXT' 'sasl.mechanism=PLAIN' > prop && \
+kafka-console-producer --producer.config=prop  --bootstrap-server localhost:9092 --topic events" < answer.json
+
+echo "Submit sent"
