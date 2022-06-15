57b5c21fbabdd3172401223c14e3976a3580bdc2
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-06-15 14:31:33 +1000

    Junit tests adeed

---
+++ messages/src/main/java/life/genny/messages/tests/MessageSender.java
@@ -0,0 +1,13 @@
+package life.genny.messages.tests;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class MessageSender {
+
+    @Test
+    public void simpleTest() {
+        System.out.println("Simple test");
+    }
+}
