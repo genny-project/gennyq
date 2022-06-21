929d4e03dcb6fa935660731afd3ec129093f91d3
Adam Crow <adamcrow63@gmail.com>
2022-06-22 08:58:03 +1000

    Hide the WARN  [org.infinispan.protostream.impl.ProtoStreamReaderImpl] (529) IPROTO000001

--- kogitoq/gadaq/scripts/process-questions.sh
+++ kogitoq/gadaq/scripts/process-questions.sh
@@ -11,7 +11,7 @@ curl -s   -H "Content-Type: application/json"  -H "Accept: application/json" -H 
 {
 	"questionCode" : "${questionCode}",
 	"sourceCode"   : "PER_086CDF1F-A98F-4E73-9825-0A4CFE2BB943",
-	"targetCode"   : "PER_34EB0455-1DC0-4121-80ED-90C0B9EEA413",
+	"targetCode"   : "APP_FFFEE841-0FD3-4C38-83E6-F3C26784C32C",
 	"pcmCode"      : "PCM_INTERN",
 	"userTokenStr" : "${TOKEN}"
 }
