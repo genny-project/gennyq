e117f7896949d53cf7e53b63db10275fba44c828
Jasper Robison <jasper.robison22@gmail.com>
2022-06-27 12:05:10 +1000

    fix process_questions script

--- kogitoq/gadaq/scripts/process-questions.sh
+++ kogitoq/gadaq/scripts/process-questions.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+if [ "$#" -lt 3 ]; then
+	echo "Usage: $0 <productCode> <questionCode> <targetCode> (optional <port>)"
+	exit 1
+fi
 productCode=${1}
 questionCode=${2}
 targetCode=${3}
 port="${4:-6590}"
 TOKEN=`./gettoken-cache.sh ${productCode}`
 
-curl -X POST "http://alyson2.genny.life:${port}/processQuestions"  -H "Content-Type: application/json"  -H "Accept: application/json" -H "Authorization: Bearer $TOKEN" -d @- << EOF
-{
-	"questionCode" : "${questionCode}",
-	"sourceCode"   : "PER_0F6169E1-FDD5-4DAF-BEC3-4126C6626752",
-	"targetCode"   : "${targetCode}",
-	"pcmCode"      : "PCM_FORM"
-}
-EOF
+payload="\
+{\
+	\"type\":\"start_process_questions\",\
+	\"data\": {\
+		\"code\": \"${questionCode}\",\
+		\"sourceCode\": \"PER_0F6169E1-FDD5-4DAF-BEC3-4126C6626752\",\
+		\"targetCode\": \"${targetCode}\",\
+		\"parentCode\": \"PCM_FORM\",\
+		\"token\":\"${TOKEN}\"\
+	}\
+}"
+
+echo $payload > msg.json
+
+docker exec -i kafka bash -c " \
+printf '%s\n%s\n' 'security.protocol=PLAINTEXT' 'sasl.mechanism=PLAIN' > prop && \
+kafka-console-producer --producer.config=prop  --bootstrap-server localhost:9092 --topic start_process_questions" < msg.json
