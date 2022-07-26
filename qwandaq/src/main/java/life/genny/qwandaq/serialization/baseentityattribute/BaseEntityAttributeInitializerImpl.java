76898c2830b124c513049033c9638ea07c7612b5
varunshastry <varunshastry@rediffmail.com>
2022-07-26 14:56:00 +0530

    Initial commit

---
+++ qwandaq/src/main/java/life/genny/qwandaq/serialization/baseentityattribute/BaseEntityAttributeInitializerImpl.java
@@ -0,0 +1,33 @@
+package life.genny.qwandaq.serialization.baseentityattribute;
+
+import java.io.UncheckedIOException;
+
+import org.infinispan.protostream.FileDescriptorSource;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.SerializationContextInitializer;
+import org.infinispan.protostream.impl.ResourceUtils;
+
+public class BaseEntityAttributeInitializerImpl implements SerializationContextInitializer {
+
+	@Override
+	public String getProtoFileName() {
+		return "/life/genny/qwandaq/serialization/protos/baseentity_attribute.proto";
+	}
+
+	@Override
+	public String getProtoFile() throws UncheckedIOException {
+		return ResourceUtils.getResourceAsString(getClass(), getProtoFileName());
+	}
+
+	@Override
+	public void registerSchema(SerializationContext serCtx) {
+		serCtx.registerProtoFiles(FileDescriptorSource.fromString(getProtoFileName(), getProtoFile()));
+
+	}
+
+	@Override
+	public void registerMarshallers(SerializationContext serCtx) {
+		serCtx.registerMarshaller(new BaseEntityAttributeMessageMarshaller());
+	}
+
+}
---
+++ qwandaq/src/main/java/life/genny/qwandaq/serialization/baseentityattribute/BaseEntityAttributeKeyMessageMarshaller.java
@@ -0,0 +1,34 @@
+package life.genny.qwandaq.serialization.baseentityattribute;
+
+import java.io.IOException;
+
+import org.infinispan.protostream.MessageMarshaller;
+
+public class BaseEntityAttributeKeyMessageMarshaller implements MessageMarshaller<BaseEntityAttributeKey> {
+
+	@Override
+	public Class<BaseEntityAttributeKey> getJavaClass() {
+		return BaseEntityAttributeKey.class;
+	}
+
+	@Override
+	public String getTypeName() {
+		return "life.genny.qwandaq.serialization.baseentity.BaseEntityAttributeKey";
+	}
+
+	@Override
+	public BaseEntityAttributeKey readFrom(ProtoStreamReader reader) throws IOException {
+		String productCode = reader.readString("realm");
+		String baseEntityCode = reader.readString("baseentitycode");
+		String attributeCode = reader.readString("attributecode");
+		return new BaseEntityAttributeKey(productCode, baseEntityCode, attributeCode);
+	}
+
+	@Override
+	public void writeTo(ProtoStreamWriter writer, BaseEntityAttributeKey beak) throws IOException {
+		writer.writeString("realm", beak.getRealm());
+		writer.writeString("baseentitycode", beak.getBaseEntityCode());
+		writer.writeString("attributecode", beak.getAttributeCode());
+	}
+
+}
