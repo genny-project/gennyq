502143c666356d08c649c52df5f41274b80b740b
Jasper Robison <jrobison@internmatchadmins-MacBook-Pro.local>
2022-08-09 15:08:49 +1000

    add be utils with todos

---
+++ qwandaq/src/test/java/life/genny/qwandaq/utils/BaseEntityUtilsTest.java
@@ -0,0 +1,27 @@
+package life.genny.qwandaq.utils;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseEntityUtilsTest {
+
+	static Jsonb jsonb = JsonbBuilder.create();
+
+	@Test
+	public void nullInputTest()
+	{
+		// TODO
+	}
+
+	// TODO: Get Code Array
+
+	// TODO: Clean Attribute Value
+
+	// TODO: Create BaseEntity Test
+
+	// TODO: Update BaseEntity Test
+
+	// TODO: Privacy Filter Test
+}
