e7bd3fa6eace5dad2d9ae31f1573874090ec084f
Jasper Robison <jrobison@GA-PM-BCD0747082CF.localdomain>
2022-08-15 17:10:14 +1000

    testing out mockito

---
+++ qwandaq/src/test/java/life/genny/test/qwandaq/utils/DatabaseUtilsTest.java
@@ -0,0 +1,20 @@
+package life.genny.test.qwandaq.utils;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DatabaseUtilsTest {
+
+	static Jsonb jsonb = JsonbBuilder.create();
+
+	@Test
+	public void baseEntityTest()
+	{
+	}
+
+}
