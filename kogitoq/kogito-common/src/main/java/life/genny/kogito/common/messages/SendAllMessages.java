c33231a1939b5b3fd5fbfb9c8fb599846b3d9250
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-29 16:13:06 +1000

    functions changed to void

--- kogitoq/kogito-common/src/main/java/life/genny/kogito/common/messages/SendAllMessages.java
+++ kogitoq/kogito-common/src/main/java/life/genny/kogito/common/messages/SendAllMessages.java
@@ -18,9 +18,12 @@ public class SendAllMessages extends MessageSendingStrategy {
     @Inject
     SearchUtils searchUtils;
 
-    private String productCode;
-    private String milestoneCode;
-    private BaseEntity coreBE;
+    private final String productCode;
+    private final String milestoneCode;
+    private final BaseEntity coreBE;
+
+    private final Map<String, String> ctxMap = new HashMap<>();
+    private String recipientBECode = null;
 
     public static final String SBE_MILESTONE_MESSAGES = "SBE_MILESTONE_MESSAGES";
     public static final String NAME = "Fetch All Messages associated with milestone Code";
@@ -28,6 +31,8 @@ public class SendAllMessages extends MessageSendingStrategy {
     public static final String PRI_MILESTONE = "SBE_MILESTONE_MESSAGES";
     public static final String PRI_RECIPIENT_LNK = "PRI_RECIPIENT_LNK";
     public static final String PRI_SENDER_LNK = "PRI_SENDER_LNK";
+    public static final String RECIPIENT = "RECIPIENT";
+    public static final String SENDER = "SENDER";
     public SendAllMessages(String productCode, String milestoneCode, BaseEntity coreBE) {
         this.productCode = productCode;
         this.milestoneCode = milestoneCode;
@@ -57,15 +62,11 @@ public class SendAllMessages extends MessageSendingStrategy {
             messageCodes.parallelStream().forEach((messageCode) -> {
                 log.info("messageCode : " + messageCode);
                 BaseEntity message = beUtils.getBaseEntityByCode(messageCode);
-                // Construct a contextMap
-                final Map<String, String> ctxMap = new HashMap<>();
-                Map<String, String> senderCtxMap = null;
-                String recipientBECode = null;
 
                 // Determine the recipientBECode
                 String recipientLnkValue = message.getValueAsString(PRI_RECIPIENT_LNK);
                 if(recipientLnkValue != null) {
-                    recipientBECode = determineRecipientLnkValue(recipientLnkValue, ctxMap);
+                    determineRecipientLnkValueAndUpdateMap(recipientLnkValue);
                 } else {
                     log.error("NO " + PRI_RECIPIENT_LNK + " present");
                 }
@@ -85,30 +86,28 @@ public class SendAllMessages extends MessageSendingStrategy {
                 // Determine the sender
                 String senderLnkValue = message.getValueAsString(PRI_SENDER_LNK);
                 if (senderLnkValue != null) {
-                    senderCtxMap = determineSender(senderLnkValue, ctxMap);
+                    determineSenderAndUpdateMap(senderLnkValue);
                 } else {
                     log.error("NO " + PRI_SENDER_LNK + " present");
                 }
 
                 log.info("Sending Message " + message.getCode() + " to " + recipientBECode + " with ctx=" + contextMapStr);
 
-                new SendMessage(message.getCode(), recipientBECode, senderCtxMap).sendMessage();
+                new SendMessage(message.getCode(), recipientBECode, ctxMap).sendMessage();
             });
         } else {
             log.warn("No messages found for milestoneCode -> " + milestoneCode);
         }
     }
 
-    private String determineRecipientLnkValue(String recipientLnkValue, Map<String, String> ctxMap) {
-        String recipientBECode = null;
-
+    private void determineRecipientLnkValueAndUpdateMap(String recipientLnkValue) {
         if (recipientLnkValue != null) {
             // check the various formats to get the recipientBECode
             if (recipientLnkValue.startsWith("SELF")) { // The coreBE is the recipient
-                ctxMap.put("RECIPIENT", coreBE.getCode());
+                ctxMap.put(RECIPIENT, coreBE.getCode());
                 recipientBECode = coreBE.getCode();
             } else if (recipientLnkValue.startsWith("PER_")) {
-                ctxMap.put("RECIPIENT", recipientLnkValue);
+                ctxMap.put(RECIPIENT, recipientLnkValue);
                 recipientBECode = recipientLnkValue;
             } else {
                 // check if it is a LNK path (of the coreBE)
@@ -119,22 +118,20 @@ public class SendAllMessages extends MessageSendingStrategy {
                     for (String s : splitStr) {
                         lnkBe = beUtils.getBaseEntityFromLinkAttribute(lnkBe, s);
                     }
-                    ctxMap.put("RECIPIENT", lnkBe.getCode());
+                    ctxMap.put(RECIPIENT, lnkBe.getCode());
                     recipientBECode = lnkBe.getCode();
                 }
             }
         }
-
-        return recipientBECode;
     }
 
-    private Map<String, String> determineSender(String senderLnkValue, Map<String, String> ctxMap) {
+    private void determineSenderAndUpdateMap(String senderLnkValue) {
         if (senderLnkValue != null) {
             // check the various formats to get the senderBECode
             if (senderLnkValue.startsWith("USER")) { // The user is the sender
-                ctxMap.put("SENDER", userToken.getUserCode());
+                ctxMap.put(SENDER, userToken.getUserCode());
             } else if (senderLnkValue.startsWith("PER_")) {
-                ctxMap.put("SENDER", senderLnkValue);
+                ctxMap.put(SENDER, senderLnkValue);
             } else {
                 // check if it is a LNK path (of the coreBE)
                 // "LNK_INTERN"
@@ -144,11 +141,9 @@ public class SendAllMessages extends MessageSendingStrategy {
                     for (String s : splitStr) {
                         lnkBe = beUtils.getBaseEntityFromLinkAttribute(lnkBe, s);
                     }
-                    ctxMap.put("SENDER", lnkBe.getCode());
+                    ctxMap.put(SENDER, lnkBe.getCode());
                 }
             }
         }
-
-        return ctxMap;
     }
 }
