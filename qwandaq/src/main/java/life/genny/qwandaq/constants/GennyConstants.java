dec7e221b677f6dc0fd09e97170acf24a73867d4
weslyvo81 <108639650+weslyvo81@users.noreply.github.com>
2022-08-30 12:07:09 +1000

    Refactoring sorting, search and pagination (#53)

--- qwandaq/src/main/java/life/genny/qwandaq/constants/GennyConstants.java
+++ qwandaq/src/main/java/life/genny/qwandaq/constants/GennyConstants.java
@@ -4,4 +4,19 @@ public final class GennyConstants {
     public static final String CACHE_NAME_BASEENTITY = "baseentity";
     public static final String CACHE_NAME_BASEENTITY_ATTRIBUTE = "baseentity_attribute";
     public static final String PACKAGE_PREFIX = "life.genny";
+
+    public static final String PAGINATION_NEXT="QUE_TABLE_NEXT_BTN";
+    public static final String PAGINATION_PREV="QUE_TABLE_PREVIOUS_BTN";
+    public static final String PAGINATION_INDEX="PRI_INDEX";
+    public static final String SEARCH_TEXT="PRI_SEARCH_TEXT";
+    public static final String SEARCH_NAME="PRI_NAME";
+    public static final String PCM_TABLE = "PCM_TABLE";
+    public static final String PCM_PROCESS = "PCM_PROCESS";
+    public static final String BUCKET_CODES = "BUCKET_CODES";
+    public static final String BUCKET_DISPLAY = "DISPLAY";
+    public static final String BUCKET_PROCESS = "PROCESS";
+
+    public static final String EVENT_WEBCMDS = "webcmds";
+    public static final String EVENT_SEARCH = "search_events";
+
 }
