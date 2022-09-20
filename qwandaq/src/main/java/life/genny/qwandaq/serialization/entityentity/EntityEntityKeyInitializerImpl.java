5bc597d475855ca56180b1a49ddb228bd2e41b7f
varunshastry <varunshastry@rediffmail.com>
2022-09-20 19:48:51 +0530

    BEA, Attribute, Question caching changes

---
+++ qwandaq/src/main/java/life/genny/qwandaq/serialization/entityentity/EntityEntityKeyInitializerImpl.java
@@ -0,0 +1,34 @@
+package life.genny.qwandaq.serialization.entityentity;
+
+import java.io.UncheckedIOException;
+
+import org.infinispan.protostream.FileDescriptorSource;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.SerializationContextInitializer;
+import org.infinispan.protostream.impl.ResourceUtils;
+
+public class EntityEntityKeyInitializerImpl implements SerializationContextInitializer {
+
+	@Override
+	public String getProtoFileName() {
+		// return "/life/genny/qwandaq/serialization/protos/baseentity_attribute_key.proto";
+		return "baseentity_attribute_key.proto";
+	}
+
+	@Override
+	public String getProtoFile() throws UncheckedIOException {
+		return ResourceUtils.getResourceAsString(getClass(), "/life/genny/qwandaq/serialization/protos/" + getProtoFileName());
+	}
+
+	@Override
+	public void registerSchema(SerializationContext serCtx) {
+		serCtx.registerProtoFiles(FileDescriptorSource.fromString(getProtoFileName(), getProtoFile()));
+
+	}
+
+	@Override
+	public void registerMarshallers(SerializationContext serCtx) {
+		serCtx.registerMarshaller(new EntityEntityKeyMessageMarshaller());
+	}
+
+}
