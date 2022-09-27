0a73f579fd8fc7e776fe186d14cba1ff640ee57e
wayanMastra <wayan.mastra@onebyone.co>
2022-09-27 17:33:26 +0800

    fixing merge

--- qwandaq/src/main/java/life/genny/qwandaq/utils/QwandaUtils.java
+++ qwandaq/src/main/java/life/genny/qwandaq/utils/QwandaUtils.java
@@ -2,6 +2,7 @@ package life.genny.qwandaq.utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
