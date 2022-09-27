1094102b4c8d49d946e7aa08e75007ff922f2c70
Adam Crow <adamcrow63@gmail.com>
2022-09-27 22:41:57 +1000

    Updated GithubUtils

--- qwandaq/src/main/java/life/genny/qwandaq/utils/GithubUtils.java
+++ qwandaq/src/main/java/life/genny/qwandaq/utils/GithubUtils.java
@@ -179,12 +179,12 @@ public class GithubUtils {
             throws InvalidRemoteException, TransportException, GitAPIException,
             RevisionSyntaxException, AmbiguousObjectException, IncorrectObjectTypeException, IOException {
 
-        List<BaseEntity> layouts = new ArrayList<BaseEntity>();
+        List<BaseEntity> layouts = new ArrayList<>();
 
-        Map<String, String> lays = new HashMap<String, String>();
+        Map<String, String> lays = new HashMap<>();
 
         String gitFolder = gitrealm;
-        String realmFilter = gitFolder;// +"/sublayouts";
+        String realmFilter = gitFolder;
 
         log.info("remoteUrl=" + remoteUrl);
         log.info("branch=" + branch);
@@ -193,8 +193,7 @@ public class GithubUtils {
         log.info("gitFolder=" + gitFolder);
         log.info("realmFilter=" + realmFilter);
 
-        // Process pp = Runtime.getRuntime().exec("cd /tmp;git clone -b "+branch+"
-        // "+remoteUrl);
+
 
         String tmpDir = "/tmp/git";
         try {
