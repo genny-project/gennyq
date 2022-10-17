cd80b714c7c72d51ebffdc75cb47c4a9032ecd4d
Jasper Robison <jasper.robison22@gmail.com>
2022-10-17 12:23:21 +1100

    udpate comms

--- qwandaq/src/main/java/life/genny/qwandaq/message/QCmd.java
+++ qwandaq/src/main/java/life/genny/qwandaq/message/QCmd.java
@@ -4,13 +4,24 @@ public class QCmd extends QMessage {
 
 	private static final String MSG_TYPE = "CMD";
 
+	private String command;
+
 	public QCmd() {
 		super(MSG_TYPE);
 	}
 
-	public QCmd(String code) {
+	public QCmd(String command, String code) {
 		this();
+		this.command = command;
 		setCode(code);
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public void setCommand(String command) {
+		this.command = command;
 	}
 	
 }
--- qwandaq/src/main/java/life/genny/qwandaq/message/QCommunication.java
+++ qwandaq/src/main/java/life/genny/qwandaq/message/QCommunication.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import java.util.HashMap;
 
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.inject.Inject;
+
 import org.jboss.logging.Logger;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
@@ -20,238 +23,66 @@ public class QCommunication extends QMessage {
 
 	private static final Logger log = Logger.getLogger(QCommunication.class);
 	private static final long serialVersionUID = 1L;
+
 	private static final String MSG_TYPE = "COMMUNICATION";
+
 	private String templateCode;
-	private QCommunicationType[] messageTypeArr;
-	private String[] recipientArr;
-	private Map<String, String> messageContextMap;
+	private List<String> recipients;
+	private List<QCommunicationType> communicationTypes;
+	private Map<String, String> contexts;
+
+	@Inject
+	BaseEntityUtils beUtils;
 	
-	/**
-	 * @return the templateCode
-	 */
-	public String getTemplateCode() {
-		return templateCode;
-	}
-
-	/**
-	 * @param templateCode the templateCode to set
-	 */
-	public void setTemplateCode(String templateCode) {
-		this.templateCode = templateCode;
-	}
-
-	/**
-	 * @return QBaseMSGMessageType the messageTypeArr
-	 */
-	public QCommunicationType[] getMessageTypeArr() {
-		return messageTypeArr;
-	}
-
-	/**
-	 * @param messageTypeArr the messageTypeArr to set
-	 */
-	public void setMessageTypeArr(QCommunicationType[] messageTypeArr) {
-		this.messageTypeArr = messageTypeArr;
-	}
-
-	/**
-	 * @return String the recipientArr
-	 */
-	public String[] getRecipientArr() {
-		return recipientArr;
-	}
-
-	/**
-	 * @param recipientArr the recipientArr to set
-	 */
-	public void setRecipientArr(String[] recipientArr) {
-		this.recipientArr = recipientArr;
-	}
-
-	/**
-	 * @return Map the messageContextMap
-	 */
-	public Map<String, String> getMessageContextMap() {
-		return messageContextMap;
-	}
-
-	/**
-	 * @param messageContextMap the messageContextMap to set
-	 */
-	public void setMessageContextMap(Map<String, String> messageContextMap) {
-		this.messageContextMap = messageContextMap;
-	}
-	
-	public QCommunication() {
-		super("COM_MSG");
-		this.messageTypeArr = new QCommunicationType[0];
-		this.messageContextMap = new HashMap<String, String>();
-		this.recipientArr = new String[0];
-	}
-
-	public QCommunication(String templateCode) {
-		super("COM_MSG");
-		this.templateCode = templateCode;
-		this.messageTypeArr = new QCommunicationType[0];
-		this.messageContextMap = new HashMap<String, String>();
-		this.recipientArr = new String[0];
-	}
-
-	public QCommunication(QCommunicationType messageType) {
-		super("COM_MSG");
-		this.messageTypeArr = new QCommunicationType[]{ messageType };
-		this.messageContextMap = new HashMap<String, String>();
-		this.recipientArr = new String[0];
-	}
-
-	public QCommunication(String msg_type, QCommunicationType[] messageType, String templateCode, Map<String, String> contextMap, String[] recipientArr) {
-		super(msg_type);
-		this.templateCode = templateCode;
-		this.messageTypeArr = messageType;
-		this.messageContextMap = contextMap;
-		this.recipientArr = recipientArr;
-	}
-	
-	/** 
-	 * @param messageType the type of message to set
-	 */
-	public void addMessageType(QCommunicationType messageType) {
-		
-		List<QCommunicationType> list = this.getMessageTypeArr() != null ? new CopyOnWriteArrayList<>(Arrays.asList(this.getMessageTypeArr())) : new CopyOnWriteArrayList<>();
-		list.add(messageType);
-		this.setMessageTypeArr(list.toArray(new QCommunicationType[0]));
-	}
-
-	/** 
-	 * @param recipient the entity of the recipient to set
-	 */
-	public void setRecipient(String recipient) {
-		this.recipientArr = new String[0];
-		addRecipient(recipient);
-	}
-
-	/** 
-	 * @param recipient the code or email of the recipient to set
-	 */
-	public void setRecipient(BaseEntity recipient) {
-		this.recipientArr = new String[0];
-		addRecipient(recipient);
-	}
-
-	/** 
-	 * @param recipient the entity of the recipient to add
-	 */
-	public void addRecipient(BaseEntity recipient) {
-		if (recipient == null) {
-			log.warn("RECIPIENT BE passed is NULL");
-		} else {
-			addRecipient("[\""+recipient.getCode()+"\"]");
-		}
-	}
-
-	/** 
-	 * @param recipient the code or email of the recipient to add
-	 */
-	public void addRecipient(String recipient) {
-		
-		if (recipient == null) {
-			log.warn("RECIPIENT passed is NULL");
-		} else {
-			List<String> list = this.getRecipientArr() != null ? new CopyOnWriteArrayList<>(Arrays.asList(this.getRecipientArr())) : new CopyOnWriteArrayList<>();
-			list.add(recipient);
-			this.setRecipientArr(list.toArray(new String[0]));
-		}
-	}
-	
-	/** 
-	 * @param key the key of the context to add
-	 * @param value the value of the context to add
-	 */
-	public void addContext(String key, Object value) {
-		if (value == null) {
-			log.warn(key + " passed is NULL");
-		} else {
-			if (value.getClass().equals(BaseEntity.class)) {
-				this.messageContextMap.put(key, ((BaseEntity) value).getCode());
-			} else {
-				this.messageContextMap.put(key, value.toString());
-			}
-		}
-	}
-
 	/* 
 	 * Thought it unnecessary to rewrite all of these methods, 
 	 * so the builder re-uses them instead.
 	 */
 	public QCommunication(Builder builder) {
-		super("COM_MSG");
-		this.templateCode = builder.msg.templateCode;
-		this.messageTypeArr = builder.msg.messageTypeArr;
-		this.recipientArr = builder.msg.recipientArr;
-		this.messageContextMap = builder.msg.messageContextMap;
+		super(MSG_TYPE);
+		this.templateCode = builder.templateCode;
+		this.communicationTypes = builder.communicationTypes;
+		this.recipients = builder.recipients;
+		this.contexts = builder.contexts;
 	}
 
 	public static class Builder {
 
-		public QCommunication msg;
-		public BaseEntityUtils beUtils;
+		private String templateCode;
+		private List<String> recipients;
+		private List<QCommunicationType> communicationTypes;
+		private Map<String, String> contexts;
 
 		public Builder(final String templateCode) {
-			this.msg = new QCommunication(templateCode);
-		}
-
-		public Builder(final String templateCode, BaseEntityUtils beUtils) {
-			this.msg = new QCommunication(templateCode);
-			this.beUtils = beUtils;
+			this.templateCode = templateCode;
 		}
 
 		public Builder addRecipient(BaseEntity recipient) {
-			this.msg.addRecipient(recipient);
+			this.recipients.add(recipient.getCode());
 			return this;
 		}
 
 		public Builder addRecipient(String recipient) {
-			this.msg.addRecipient(recipient);
+			this.recipients.add(recipient);
 			return this;
 		}
 
-		public Builder setMessageContextMap(Map<String, String> ctxMap) {
-			this.msg.setMessageContextMap(ctxMap);
+		public Builder setMessageContextMap(Map<String, String> contexts) {
+			this.contexts = contexts;
 			return this;
 		}
 
 		public Builder addContext(String key, Object value) {
-			this.msg.addContext(key, value);
+			this.contexts.put(key, value.toString());
 			return this;
 		}
 
-		public Builder addMessageType(QCommunicationType messageType) {
-			this.msg.addMessageType(messageType);
-			return this;
-		}
-
-		public Builder setToken(GennyToken token) {
-			this.msg.setToken(token.getToken());
-			return this;
-		}
-		
-		@Deprecated
-		public Builder setToken(String token) {
-			this.msg.setToken(token);
-			return this;
-		}
-
-		public Builder setUtils(BaseEntityUtils beUtils) {
-			this.beUtils = beUtils;
+		public Builder addCommunicationType(QCommunicationType communicationType) {
+			this.communicationTypes.add(communicationType);
 			return this;
 		}
 
 		public QCommunication send() {
-
-			if (this.msg.getToken() == null) {
-				log.error("No token set for message. Cannot send!!!");
-				return this.msg;
-			}
 
 			if (this.beUtils == null) {
 				log.error("No beUtils set for message. Cannot send!!!");
@@ -285,8 +116,8 @@ public class QCommunication extends QMessage {
 			}
 
 			// Set Msg Type to DEFAULT if none set already
-			if (this.msg.messageTypeArr.length == 0) {
-				this.msg.addMessageType(QCommunicationType.DEFAULT);
+			if (this.msg.communicationTypes.length == 0) {
+				this.msg.addCommunicationType(QCommunicationType.DEFAULT);
 			}
 
 			KafkaUtils.writeMsg(KafkaTopic.MESSAGES, this.msg);
@@ -304,8 +135,8 @@ public class QCommunication extends QMessage {
 	 */
 	@Override
 	public String toString() {
-		return "QMessageGennyMSG [templateCode=" + templateCode + ", messageTypeArr=" + messageTypeArr
-				+ ", recipientArr=" + Arrays.toString(recipientArr) + ", messageContextMap=" + messageContextMap + "]";
+		return "QMessageGennyMSG [templateCode=" + templateCode + ", messageTypeArr=" + communicationTypes
+				+ ", recipientArr=" + Arrays.toString(recipients) + ", messageContextMap=" + contexts + "]";
 	}
 
 }
