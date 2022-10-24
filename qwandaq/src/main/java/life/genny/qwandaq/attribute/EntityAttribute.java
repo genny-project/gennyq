2eb42f47ad9c8d3917f1e62394bd81b42380a61a
Expressionless <rhysmchaen@gmail.com>
2022-10-24 11:37:57 +1100

    Catering for multiple capabilities to entity attribute

--- qwandaq/src/main/java/life/genny/qwandaq/attribute/EntityAttribute.java
+++ qwandaq/src/main/java/life/genny/qwandaq/attribute/EntityAttribute.java
@@ -14,6 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -191,7 +192,7 @@ public class EntityAttribute implements java.io.Serializable, Comparable<Object>
 	// @JsonIgnore
 	@Column(name = "requirement")
 	@Convert(converter = CapabilityConverter.class)
-	private Capability requirement;
+	private Set<Capability> requirement;
 
 	public EntityAttribute() {
 
--- qwandaq/src/main/java/life/genny/qwandaq/converter/CapabilityConverter.java
+++ qwandaq/src/main/java/life/genny/qwandaq/converter/CapabilityConverter.java
@@ -1,5 +1,9 @@
 package life.genny.qwandaq.converter;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.persistence.AttributeConverter;
 
 import org.apache.commons.lang3.StringUtils;
@@ -9,30 +13,56 @@ import life.genny.qwandaq.datatype.capability.Capability;
 import life.genny.qwandaq.exception.runtime.BadDataException;
 import life.genny.qwandaq.managers.capabilities.CapabilitiesManager;
 
-public class CapabilityConverter implements AttributeConverter<Capability, String> {
+public class CapabilityConverter implements AttributeConverter<Set<Capability>, String> {
+    private static final String CAPABILITY_DELIMITER = "$_$";
 	private static final String ARRAY_START = "[";
 	private static final Logger log = Logger.getLogger(CapabilitiesManager.class);
 
     @Override
-    public String convertToDatabaseColumn(Capability attribute) {
-        if(attribute == null) {
+    public String convertToDatabaseColumn(Set<Capability> attributeSet) {
+        if(attributeSet.isEmpty()) {
             return "";
         }
-        return new StringBuilder(attribute.code).append(CapabilitiesManager.getModeString(attribute.nodes)).toString();
+
+        StringBuilder data = new StringBuilder();
+        Iterator<Capability> iterator = attributeSet.iterator();
+        for(int i = 0; i < attributeSet.size(); i++) {
+            Capability cap = iterator.next();
+            serializeOneCapability(data, cap);
+            if(iterator.hasNext())
+                data.append(CAPABILITY_DELIMITER);
+        }
+
+        return data.toString();
     }
 
     @Override
-    public Capability convertToEntityAttribute(String dbData) {
+    public Set<Capability> convertToEntityAttribute(String dbData) {
         if(StringUtils.isBlank(dbData))
-            return null;
-        int delimIndex = dbData.indexOf(ARRAY_START);
-        if(delimIndex == -1) {
-            log.error("Could not find array start in capability string: " + dbData);
-            log.error("Delimiter: " + ARRAY_START);
-            throw new BadDataException("dbData: " + dbData);
+            return new HashSet<>();
+        Set<Capability> capSet = new HashSet<>();
+        String[] capabilities = dbData.split(CAPABILITY_DELIMITER);
+        // TODO: May need to verify capability string
+        for(String cap : capabilities) {
+            capSet.add(deserializeOneCapability(cap));
         }
-        String code = dbData.substring(0, delimIndex);
-        String nodes = dbData.substring(delimIndex);
+
+        return capSet;
+    }
+
+    private Capability deserializeOneCapability(String capData) {
+        int delimIndex = capData.indexOf(ARRAY_START);
+        if(delimIndex == -1) {
+            log.error("Could not find array start in capability string: " + capData);
+            log.error("Delimiter: " + ARRAY_START);
+            throw new BadDataException("dbData: " + capData);
+        }
+        String code = capData.substring(0, delimIndex);
+        String nodes = capData.substring(delimIndex);
         return new Capability(code, nodes);
+    }
+
+    private StringBuilder serializeOneCapability(StringBuilder sb, Capability capability) {
+        return sb.append(capability.code).append(CapabilitiesManager.getModeString(capability.nodes));
     }
 }
