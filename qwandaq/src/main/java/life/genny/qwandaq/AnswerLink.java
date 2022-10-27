6e02881353490e791d90b112390e869e693f72c4
Jasper Robison <jasper.robison22@gmail.com>
2022-10-27 11:11:06 +1100

    TODO

--- qwandaq/src/main/java/life/genny/qwandaq/AnswerLink.java
+++ qwandaq/src/main/java/life/genny/qwandaq/AnswerLink.java
@@ -219,6 +219,7 @@ public class AnswerLink implements java.io.Serializable {
 
 		switch (className) {
 
+			// TODO: remove the qwanda version once db's are clear of it
 			case "life.genny.qwandaq.entity.BaseEntity":
 			case "life.genny.qwanda.entity.BaseEntity":
 				setValueString(result);
