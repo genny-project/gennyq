97b805cbbf60e13a1cf95851a4c36cf4598e5765
Expressionless <rhysmchaen@gmail.com>
2022-11-28 12:52:38 +1100

    Verif callback NPE (more robust now)

--- qwandaq/src/main/java/life/genny/qwandaq/utils/testsuite/TestCase.java
+++ qwandaq/src/main/java/life/genny/qwandaq/utils/testsuite/TestCase.java
@@ -51,7 +51,6 @@ public class TestCase<I, E> {
         try { 
             verificationCallback.assertFunction(result, expected.expected);
         } catch(AssertionError e) {
-            System.err.println("TEST FAILED: " + this);
             assert(false);
         }
     }
