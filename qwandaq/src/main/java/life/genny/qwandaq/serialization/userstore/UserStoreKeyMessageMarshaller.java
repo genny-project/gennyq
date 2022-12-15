ac3840901cf6b01194f206ee29b6a02be5db9540
varunshastry <varunshastry@rediffmail.com>
2022-12-15 13:07:06 +1100

    Adding infinispan client code for userstore

---
+++ qwandaq/src/main/java/life/genny/qwandaq/serialization/userstore/UserStoreKeyMessageMarshaller.java
@@ -0,0 +1,33 @@
+package life.genny.qwandaq.serialization.userstore;
+
+import org.infinispan.protostream.MessageMarshaller;
+
+import java.io.IOException;
+
+public class UserStoreKeyMessageMarshaller implements MessageMarshaller<UserStoreKey> {
+
+	@Override
+	public Class<UserStoreKey> getJavaClass() {
+		return UserStoreKey.class;
+	}
+
+	@Override
+	public String getTypeName() {
+		return "life.genny.qwandaq.persistence.userstore.UserStoreKey";
+	}
+
+	@Override
+	public UserStoreKey readFrom(ProtoStreamReader reader) throws IOException {
+		String productCode = reader.readString("realm");
+		String usercode = reader.readString("usercode");
+		UserStoreKey usk = new UserStoreKey(productCode, usercode);
+		return usk;
+	}
+
+	@Override
+	public void writeTo(ProtoStreamWriter writer, UserStoreKey usk) throws IOException {
+		writer.writeString("realm", usk.getRealm());
+		writer.writeString("usercode", usk.getUsercode());
+	}
+
+}
