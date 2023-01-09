180ed4f328d8d4fb7c7daa88b731d4dc3be2d5a0
Expressionless <rhysmchaen@gmail.com>
2023-01-09 12:11:43 +1100

    more copy

--- qwandaq/src/main/java/life/genny/qwandaq/intf/ICapabilityFilterable.java
+++ qwandaq/src/main/java/life/genny/qwandaq/intf/ICapabilityFilterable.java
@@ -1,7 +1,9 @@
 package life.genny.qwandaq.intf;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -9,6 +11,7 @@ import org.jboss.logging.Logger;
 
 import life.genny.qwandaq.datatype.capability.core.Capability;
 import life.genny.qwandaq.datatype.capability.core.CapabilitySet;
+import life.genny.qwandaq.datatype.capability.core.node.CapabilityMode;
 import life.genny.qwandaq.datatype.capability.core.node.CapabilityNode;
 import life.genny.qwandaq.datatype.capability.requirement.ReqConfig;
 import life.genny.qwandaq.utils.CommonUtils;
@@ -20,6 +23,40 @@ public interface ICapabilityFilterable {
     }
     
     public Set<Capability> getCapabilityRequirements();
+
+    /**
+     * Get the capability requirements for this filterable which contain at least one of these modes
+     * @param filterModes - whether or not to filter down the modes of the found capability requirements to only the supplied
+     *  modes
+     * @param modes - modes to filter by
+     * @return a subset of the capability requirements that contain at least one of the supplied capability modes
+     */
+    public default Set<Capability> getCapabilityRequirements(boolean filterModes, CapabilityMode... modes) {
+        Set<Capability> requirements = getCapabilityRequirements();
+        Set<Capability> filteredRequirements = new HashSet<>(requirements.size());
+        
+        for(Capability requirement : requirements) {
+            for(CapabilityMode mode : modes) {
+                if(requirement.hasNode(mode)) {
+                    if(filterModes) {
+                        // Grab only nodes specified in args
+                        List<CapabilityNode> nodes = new ArrayList<>(requirement.nodes.size());
+                        for(CapabilityNode node : requirement.nodes) {
+                            if(CommonUtils.isInArray(modes, node.capMode))
+                                nodes.add(node);
+                        }
+
+                        requirement = new Capability(requirement.code, nodes);
+                    }
+
+                    filteredRequirements.add(requirement);
+                    break;
+                }
+            }
+        }
+
+        return filteredRequirements;
+    }
 
     public default void setCapabilityRequirements(Capability... requirements) {
         setCapabilityRequirements(new HashSet<>(Arrays.asList(requirements)));
