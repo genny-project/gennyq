6c8e8f5b47989a7c595888819b7cfc64d3635315
Expressionless <rhysmchaen@gmail.com>
2023-01-17 16:58:36 +1100

    latest

--- qwandaq/src/main/java/life/genny/qwandaq/utils/QwandaUtils.java
+++ qwandaq/src/main/java/life/genny/qwandaq/utils/QwandaUtils.java
@@ -786,7 +786,7 @@ public class QwandaUtils {
 	 * @return Ask
 	 */
 	public Ask generateAskGroupUsingBaseEntity(BaseEntity baseEntity) {
-
+		new Exception("watch").printStackTrace();
 		// grab def entity
 		Definition definition = defUtils.getDEF(baseEntity);
 
