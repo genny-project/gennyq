0a7eb79f081d7e8a448bd5fe3eb8652544b3354c
Expressionless <rhysmchaen@gmail.com>
2023-01-19 14:54:03 +1100

    logging

--- kogitoq/gadaq/src/main/java/life/genny/gadaq/route/Events.java
+++ kogitoq/gadaq/src/main/java/life/genny/gadaq/route/Events.java
@@ -193,6 +193,7 @@ public class Events {
 		if (ACT_EDIT.equals(code)) {
 
 			if (parentCode.startsWith("SBE_")) {
+				log.debug("[!] Retrieving edit question groups for target code: " + msg.getData().getTargetCode());
 				String[] questionCodes = qwandaUtils.getEditQuestionGroups(msg.getData().getTargetCode());
 				
 				JsonObject payload = Json.createObjectBuilder()
