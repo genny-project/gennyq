2194f9447ef0278bacc3badcdc5ea523e8378fa4
Expressionless <rhysmchaen@gmail.com>
2023-01-26 13:50:49 +1100

    Building. Not Done

--- kogitoq/kogito-common/src/main/java/life/genny/kogito/common/service/EditService.java
+++ kogitoq/kogito-common/src/main/java/life/genny/kogito/common/service/EditService.java
@@ -7,12 +7,14 @@ import life.genny.qwandaq.utils.CommonUtils;
 @ApplicationScoped
 public class EditService extends KogitoService  {
     
-    public String getCurrentPcm(String pcmCodes, int currentIndex) {
+    public String getCurrentPcm(String pcmCodes, Integer currentIndex) {
         String[] pcmCodesArray = CommonUtils.getArrayFromString(pcmCodes, String.class, (str) -> str);
 
         // TODO: Replace when kogito workflow errors get revised
         if(currentIndex > pcmCodesArray.length - 1)
             return pcmCodesArray[pcmCodesArray.length - 1];
-        
+        if(currentIndex < 0)
+            return pcmCodesArray[0];
+        return pcmCodesArray[currentIndex];
     }
 }
