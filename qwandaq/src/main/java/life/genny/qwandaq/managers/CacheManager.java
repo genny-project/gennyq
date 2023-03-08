d22edb50828e5471c476441efada9eb27011d54d
Expressionless <rhysmchaen@gmail.com>
2023-03-08 12:50:38 +1100

    revisit illegalstateexception impl later

--- qwandaq/src/main/java/life/genny/qwandaq/managers/CacheManager.java
+++ qwandaq/src/main/java/life/genny/qwandaq/managers/CacheManager.java
@@ -483,9 +483,9 @@ public class CacheManager {
 
 		if(StringUtils.isBlank(conditionalCheck))
 			throw new IllegalStateException("Blank conditional check detected. This would delete all data for " + persistenceName + " in product: " + productCode + ". Stopping");
-		if(!(conditionalCheck.contains("\'") && conditionalCheck.contains("\""))) {
-			throw new IllegalStateException("Conditional Check does not have quotes. Likely malformed Conditional Check: " + conditionalCheck);
-		}
+		// if(!(conditionalCheck.contains("\'") && conditionalCheck.contains("\""))) {
+		// 	throw new IllegalStateException("Conditional Check does not have quotes. Likely malformed Conditional Check: " + conditionalCheck);
+		// }
 
 		StringBuilder sb = new StringBuilder("delete from ")
 						.append(persistenceName)
