b4018469e020ec8b27d6342b93879611db5639d6
Expressionless <rhysmchaen@gmail.com>
2023-03-14 12:34:47 +1100

    Fixed remove attribute

--- qwandaq/src/main/java/life/genny/qwandaq/managers/CacheManager.java
+++ qwandaq/src/main/java/life/genny/qwandaq/managers/CacheManager.java
@@ -550,10 +550,10 @@ public class CacheManager {
 	 * @return number of entities affected by deletion
 	 */
 	public int removeAttribute(String productCode, String code) {
-		String persistenceObject = "life.genny.qwandaq.persistence.datatype.DataType";
+		String persistenceObject = "life.genny.qwandaq.persistence.datatype.Attribute";
 		String conditional = "code = '" + code + "'";
 		String deleteQuery = constructDeleteQuery(persistenceObject, productCode, conditional);
-		return removePersistableEntities(GennyConstants.CACHE_NAME_DATATYPE, deleteQuery);
+		return removePersistableEntities(GennyConstants.CACHE_NAME_ATTRIBUTE, deleteQuery);
 	}
 
 	/**
