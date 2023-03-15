585eb03a7f3cf38c3b17f394026e1a79d2d597f4
rzldev1by1 <109568042+rzldev1by1@users.noreply.github.com>
2023-03-16 05:41:03 +0800

    Fix PRI_UUID item not found (#231)

--- qwandaq/src/main/java/life/genny/qwandaq/utils/BaseEntityUtils.java
+++ qwandaq/src/main/java/life/genny/qwandaq/utils/BaseEntityUtils.java
@@ -527,11 +527,11 @@ public class BaseEntityUtils {
 		BaseEntity item;
 		String productCode = definition.getRealm();
 		String definitionCode = definition.getCode();
-		EntityAttribute uuidEA = beaUtils.getEntityAttribute(productCode, definitionCode, Prefix.ATT_.concat(Attribute.PRI_UUID));
-		if (uuidEA != null) {
+		try {
+			EntityAttribute uuidEA = beaUtils.getEntityAttribute(productCode, definitionCode, Prefix.ATT_.concat(Attribute.PRI_UUID));
 			log.debug("Creating user base entity");
 			item = createUser(definition);
-		} else {
+		} catch(ItemNotFoundException e) {
 			String prefix = defUtils.getDefinitionPrefix(productCode, definitionCode);
 			if (StringUtils.isBlank(prefix)) {
 				throw new DefinitionException("No prefix set for the def: " + definitionCode);
