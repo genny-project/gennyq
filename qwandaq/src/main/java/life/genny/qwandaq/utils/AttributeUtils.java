17fae2600465a2aa14a4ab569145243544fd6fbe
varunshastry <varunshastry@rediffmail.com>
2023-03-21 09:47:06 +1100

    Removing unused imports

--- qwandaq/src/main/java/life/genny/qwandaq/utils/AttributeUtils.java
+++ qwandaq/src/main/java/life/genny/qwandaq/utils/AttributeUtils.java
@@ -17,8 +17,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
