b8851578e29a1cf8ffaa193348d33fdf774145b3
Expressionless <rhysmchaen@gmail.com>
2023-03-22 14:10:58 +1100

    rename callback class to make it consistent

---
+++ qwandaq/src/main/java/life/genny/qwandaq/utils/callbacks/FIGetStringCallback.java
@@ -0,0 +1,14 @@
+package life.genny.qwandaq.utils.callbacks;
+
+/**
+ * Simple functional interface to allow custom strings for stringified arrays
+ */
+public interface FIGetStringCallback<T> {
+    
+    /**
+     * Takes in an object of type T and returns a stringified representation of the object
+     * @param object The object to get string from
+     * @return A String
+     */
+    String getString(T object);
+}
