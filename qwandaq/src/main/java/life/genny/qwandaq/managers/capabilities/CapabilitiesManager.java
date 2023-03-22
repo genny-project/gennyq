d2f5991fbf13991589d317b0dcb4e7ede45b3147
Expressionless <rhysmchaen@gmail.com>
2023-03-22 19:14:14 +1100

    Added a dropdown builder QoL

--- qwandaq/src/main/java/life/genny/qwandaq/managers/capabilities/CapabilitiesManager.java
+++ qwandaq/src/main/java/life/genny/qwandaq/managers/capabilities/CapabilitiesManager.java
@@ -160,7 +160,7 @@ public class CapabilitiesManager extends Manager {
 	/**
 	 * Add a capability to a BaseEntity.
 	 * 
-	 * @param productCode The product code
+	 * @param CONFIG_PRODUCT_CODE The product code
 	 * @param target      The target entity
 	 * @param capability        The capability attribute
 	 * @param nodes       The nodes to set
