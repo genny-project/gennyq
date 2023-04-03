7a9ddf859a8ad224d3af3af23d0d0dffa7964c62
Expressionless <rhysmchaen@gmail.com>
2023-04-03 12:51:33 +1000

    banana bread example

--- kogitoq/kogito-common/src/main/java/life/genny/kogito/common/service/CapabilityService.java
+++ kogitoq/kogito-common/src/main/java/life/genny/kogito/common/service/CapabilityService.java
@@ -24,11 +24,38 @@ public class CapabilityService extends KogitoService {
         return capabilities.getUserCapabilities();
     }
 
+    /**
+     * Add a capability to the session-based User base entity
+     * @param capability - Capability code to add (Starting with {@link CAP_})
+     * @param capabilityString - String representation of the nodes to set
+     * <p><h4>Example</h4>
+     * <pre>
+     *  addCapabilityToUser("CAP_BANANA_BREAD", "[\"V:A\",\"D:A\"]");
+     * </pre>
+     * will add the capability <b>CAP_BANANA_BREAD</b> with the nodes <b>VIEW:ALL</b> and <b>DELETE:ALL to the user's base entity specifically</b>
+     * </p>
+     * <p>
+     * perhaps this means the user can now view and eat all the banana bread?
+     * </p>
+     */
     public void addCapabilityToUser(String capability, String capabilityString) {
         CapabilityNode[] capabilityNodes = CommonUtils.getArrayFromString(capabilityString, CapabilityNode.class, CapabilityNode::parseNode);
         capabilities.addCapabilityToBaseEntity(userToken.getProductCode(), userToken.getCode(), capability, capabilityNodes);
     }
 
+    /**
+     * Add a capability to the session-based User base entity
+     * @param capability - Capability code to remove (Starting with {@link CAP_})
+     * <p><h4>Example</h4>
+     * <pre>
+     *  removeCapabilityFromUser("CAP_BANANA_BREAD");
+     * </pre>
+     * will remove any nodes set for the user for the capability: <b>CAP_BANANA_BREAD</b>
+     * </p>
+     * <p>
+     * perhaps this means the user ate too much banana bread and has now been banned from it?
+     * </p>
+     */
 	public void removeCapabilityFromUser(String capability) {
 		BaseEntity userBe = beUtils.getUserBaseEntity();
         capabilities.removeCapabilityFromBaseEntity(userToken.getProductCode(), userBe, capability);
