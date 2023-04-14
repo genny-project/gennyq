a38b7bb419abc595ba37c947667ca411dee8b071
Bryn <rhysmchaen@gmail.com>
2023-04-14 16:04:39 +1000

    10.3.0 fix bootq qq (#292)
    * this'll be interesting
    
    * awesome it works

--- bootq/src/main/java/life/genny/bootq/models/BatchLoading.java
+++ bootq/src/main/java/life/genny/bootq/models/BatchLoading.java
@@ -10,6 +10,7 @@ import life.genny.qwandaq.Question;
 import life.genny.qwandaq.QuestionQuestion;
 import life.genny.qwandaq.attribute.Attribute;
 import life.genny.qwandaq.attribute.EntityAttribute;
+import life.genny.qwandaq.constants.GennyConstants;
 import life.genny.qwandaq.constants.Prefix;
 import life.genny.qwandaq.datatype.DataType;
 import life.genny.qwandaq.entity.BaseEntity;
@@ -704,6 +705,7 @@ public class BatchLoading {
             }
             
             try {
+                questionUtils.removeQuestionQuestion(realmName, parentCode, targetCode);
 				questionUtils.saveQuestionQuestion(questionQuestion);
             
                 if (count++ % LOG_BATCH_SIZE == 0)
@@ -723,5 +725,7 @@ public class BatchLoading {
         if(!loadReport.hasErrors(EReportCategoryType.QUESTION_QUESTION)) {
             loadReport.addSuccess(EReportCategoryType.QUESTION_QUESTION, successFullySaved);
         }
+
+        cm.reindexCache(GennyConstants.CACHE_NAME_QUESTIONQUESTION);
     }
 }
