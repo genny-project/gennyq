c45db614f30a0b32879d89354c5450505c284ebe
Bryn <rhysmchaen@gmail.com>
2023-04-24 14:59:27 +1000

    Logging for PR #310 (#312)
    * Logging for PR #310
    
    * further logging and catching and exception

--- kogitoq/kogito-common/src/main/java/life/genny/kogito/common/workitem/MessageWorkItemHandler.java
+++ kogitoq/kogito-common/src/main/java/life/genny/kogito/common/workitem/MessageWorkItemHandler.java
@@ -1,7 +1,6 @@
 package life.genny.kogito.common.workitem;
 
 import static life.genny.qwandaq.attribute.Attribute.LNK_MESSAGE_TYPE;
-import static life.genny.qwandaq.entity.BaseEntity.PRI_NAME;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,11 +20,7 @@ import life.genny.qwandaq.message.QMessageGennyMSG;
 import life.genny.qwandaq.models.ANSIColour;
 import life.genny.qwandaq.models.UserToken;
 import life.genny.qwandaq.utils.BaseEntityUtils;
-import life.genny.qwandaq.utils.CommonUtils;
 import life.genny.qwandaq.exception.runtime.ItemNotFoundException;
-
-import org.apache.commons.lang3.StringUtils;
-import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class MessageWorkItemHandler implements KogitoWorkItemHandler {
@@ -57,51 +52,50 @@ public class MessageWorkItemHandler implements KogitoWorkItemHandler {
 
     @Override
     public void executeWorkItem(KogitoWorkItem workItem, KogitoWorkItemManager manager) {
+        String messageCode = (String) workItem.getParameter("messageCode");
+        log.info("messageCode: " + messageCode);
+        String recipientCode = (String) workItem.getParameter("recipientCode");
+        log.info("recipientCode: " + recipientCode);
+
+        // build the context map
+        Map<String, String> ctxMap = new HashMap<>();
+        for(String parameter : workItem.getParameters().keySet()) {
+            if(parameter.startsWith(CTX)){
+                String key = parameter.replace(CTX, "");
+                ctxMap.put(key, (String) workItem.getParameters().get(parameter));
+            }
+        }
+
+        log.info("contextMap: " + ctxMap);
+
+        // find the template entity
+        BaseEntity baseEntity;
         try {
-            String messageCode = (String) workItem.getParameter("messageCode");
-            log.info("messageCode: " + messageCode);
-            String recipientCode = (String) workItem.getParameter("recipientCode");
-            log.info("recipientCode: " + recipientCode);
+            baseEntity = beUtils.getBaseEntity(messageCode);
+        } catch (ItemNotFoundException e) {
+            log.error("Message template not found: " + messageCode + "! Details: " + e.getMessage());
+            return;
+        }
 
-            // build the context map
-            Map<String, String> ctxMap = new HashMap<>();
-            for(String parameter : workItem.getParameters().keySet()) {
-                if(parameter.startsWith(CTX)){
-                    String key = parameter.replace(CTX, "");
-                    ctxMap.put(key, (String) workItem.getParameters().get(parameter));
-                }
+        // find the message type selection
+        BaseEntity messageType;
+        try {
+            messageType = beUtils.getBaseEntityFromLinkAttribute(baseEntity, LNK_MESSAGE_TYPE, true);
+            log.debug("messageType: " + messageType.getCode());
+        } catch (ItemNotFoundException e) {
+            log.error("Message Type (" + LNK_MESSAGE_TYPE + ") not found in: " + messageCode + "! Details: " + e.getMessage());
+            return;
+        }
+
+        // add valid message types
+        QMessageGennyMSG.Builder msgBuilder = new QMessageGennyMSG.Builder(messageCode);
+        for (QBaseMSGMessageType type : QBaseMSGMessageType.values()) {
+            if (messageType.getCode().contains(type.name())) {
+                msgBuilder.addMessageType(type);
             }
-            log.info("contextMap: "+ ctxMap);
+        }
 
-            // find the template entity
-            BaseEntity baseEntity;
-            try {
-                baseEntity = beUtils.getBaseEntity(messageCode);
-            } catch (ItemNotFoundException e) {
-                log.error("Message template not found!");
-                e.printStackTrace();
-                return;
-            }
-
-            // find the message type selection
-            BaseEntity messageType;
-            try {
-                messageType = beUtils.getBaseEntityFromLinkAttribute(baseEntity, LNK_MESSAGE_TYPE, true);
-                log.debug("messageType: "+messageType.getCode());
-            } catch (ItemNotFoundException e) {
-                log.error("Message type not found!");
-                e.printStackTrace();
-                return;
-            }
-
-            // add valid message types
-            QMessageGennyMSG.Builder msgBuilder = new QMessageGennyMSG.Builder(messageCode);
-            for (QBaseMSGMessageType type : QBaseMSGMessageType.values()) {
-                if (messageType.getCode().contains(type.name())) {
-                    msgBuilder.addMessageType(type);
-                }
-            }
-
+        try {
             log.info("Sending message: " + messageCode);
             msgBuilder.setMessageContextMap(ctxMap);
             msgBuilder.addRecipient(recipientCode)
@@ -111,15 +105,15 @@ public class MessageWorkItemHandler implements KogitoWorkItemHandler {
                     .send();
 
             log.info("Triggered message!");
-            log.info(ANSIColour.doColour("messageCode: "+ messageCode, ANSIColour.GREEN));
-            log.info(ANSIColour.doColour("recipientCode: "+ recipientCode, ANSIColour.GREEN));
-            log.info(ANSIColour.doColour("messageType: "+ messageType, ANSIColour.GREEN));
-            log.info(ANSIColour.doColour("ctxMap: "+ ctxMap, ANSIColour.GREEN));
+            log.info(ANSIColour.doColour("messageCode: " + messageCode, ANSIColour.GREEN));
+            log.info(ANSIColour.doColour("recipientCode: " + recipientCode, ANSIColour.GREEN));
+            log.info(ANSIColour.doColour("messageType: " + messageType, ANSIColour.GREEN));
+            log.info(ANSIColour.doColour("ctxMap: " + ctxMap, ANSIColour.GREEN));
 
             Map<String, Object> results = new HashMap<String, Object>();
             manager.completeWorkItem(workItem.getStringId(), results);
-        } catch (Exception ex) {
-            log.error("Exception: " + ex);
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
